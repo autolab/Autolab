899bd7120cf9d23e8dd3ffb87b1d85b41fd4ac65
Yiming Zong <yzong@cmu.edu>
2014-11-03 00:19:44 -0500

    Use sass to compile CSS files.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,6 +16,7 @@ Autolab3::Application.configure do
   config.assets.enabled = true
   # Compress JavaScripts and CSS
   config.assets.js_compressor = :uglifier
+  config.assets.css_compressor = :sass
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
