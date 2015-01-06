c23d80d56e084947e5718c03016224eefef23b0a
Jimmy Zong <yzong@cmu.edu>
2015-01-06 13:10:53 -0500

    Merge back factories from Ilter.

--- spec/factories/assessments.rb
+++ spec/factories/assessments.rb
@@ -1,6 +1,20 @@
 FactoryGirl.define do
   factory :assessment do
-    
-  end
+    due_date { 2.weeks.from_now.to_s(:db) }
+    submit_until { (2.weeks.from_now + 2.days).to_s(:db) }
+    visible_at { 1.days.ago.to_s(:db) }
+    start_date { visible_at }
 
+    sequence(:name) { |n| "assessment_#{n}" }
+    sequence(:display_name) { |n| "Assessment #{n}" }
+
+    # We still want these so that assessments can have submissions
+    # but they should always be mocked unless we're explicity
+    # testing file system interaction.
+    handin_filename "placeholder"
+    handin_directory "placeholder"
+
+    created_at { 2.weeks.ago.to_s(:db) }
+    updated_at { 1.weeks.ago.to_s(:db) }
+  end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,9 +1,16 @@
 FactoryGirl.define do
   factory :user do
-    sequence(:email){|n| "user#{n}@foo.bar" }
-    password        "AutolabProject"
-    sequence(:first_name){|n| "#{n}" }
-    last_name       "User"
-    confirmed_at    Time.now
+    first_name "Test"
+    sequence (:last_name) { |n| "User #{n}" }
+    sequence (:andrewID) { |n| "test#{n}" }
+    nickname "Test User"
+    sequence (:email) { |n| "test#{n}@andrew.cmu.edu" }
+    confirmed_at Time.now
+
+    factory :instructor do
+      instructor true
+      administrator true
+      course_assistant false
+    end
   end
 end
