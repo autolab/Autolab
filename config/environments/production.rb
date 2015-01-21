ce0d30540e81a1fba153d3243cf8c5bc50099347
Xuetao Guan <epip.guan@gmail.com>
2015-01-21 13:14:29 -0500

    Started using rack-ssl-enforcer gem for forcing SSL

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -32,7 +32,9 @@ Autolab3::Application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  config.force_ssl = true
+  # config.force_ssl = true
+  config.middleware.use Rack::SslEnforcer, :except => [ /unofficial_submit/ ]
+
 
   # See everything in the log (default is :info)
   # config.log_level = :debug
