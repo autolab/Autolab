841079cc4615be5311c157a1728bd66e1917fd6f
Jacob Zimmerman <zimmerman.jake@gmail.com>
2015-01-31 18:13:48 -0500

    Add text to datetimepicker buttons (#181)

--- app/assets/javascripts/initialize_datetimepickers.js
+++ app/assets/javascripts/initialize_datetimepickers.js
@@ -1,6 +1,11 @@
 // Initialize all Bootstrap 3 Datetimepickers on the page
 $(document).ready(function() {
   $('.datetimepicker').datetimepicker({
-    showTodayButton: true
+    showTodayButton: true,
+    icons: {
+      time: 'glyphicon glyphicon-time time-btn',
+      date: 'glyphicon glyphicon-calendar date-btn',
+      today: 'glyphicon glyphicon-screenshot now-btn'
+    }
   });
 });
