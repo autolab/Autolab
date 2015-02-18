770549fb2c1be980fc5dee7b9db267daada29307
Ilter Canberk <iltercanberk@gmail.com>
2015-02-18 00:00:31 -0500

    Fixes the form elements on edit assessment + makes analytics optional

--- app/assets/javascripts/app-level.js
+++ app/assets/javascripts/app-level.js
@@ -1,9 +1,19 @@
 /* JavaScript code that will be added to every page throughout the application */
 
+
+/* Adding CSRF token to every Ajax call by default */
+
+jQuery.ajaxSetup({
+    headers: {
+        'X-CSRF-Token' : $('meta[name="csrf-token"]').attr('content')
+    },
+});
+
+
 /* Moment JS generic converter */
-$(".moment-time").each(function(ind) {
+$(".moment-date-time").each(function(ind) {
 	$el = $(this);
-	var format = $el.data("format");
+	var format = $el.data("format")  || "dddd, MMM Do YYYY, h:mm:ss a";
 	var unformattedDate = $el.html();
 	var formattedDate = moment(new Date(unformattedDate)).format(format);
 	$el.html(formattedDate);
