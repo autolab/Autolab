7f6ae65d7feeb63baf62178acb9d696789a2bfef
Daniel Bucci <dlbucci@superftc.com>
2015-02-19 23:00:30 -0500

    Updated (but did not test) local and log submit

--- app/controllers/assessment/handin.rb
+++ app/controllers/assessment/handin.rb
@@ -24,7 +24,7 @@ module AssessmentHandin
 
     # save the submissions
     begin
-      submissions = saveHandin
+      submissions = saveHandin(params[:submission])
     rescue Exception => e
       submissions = nil
     end
@@ -46,6 +46,201 @@ module AssessmentHandin
     redirect_to [:history, @course, @assessment] and return
   end
   
+  # method called when student makes
+  # unofficial submission in the database
+  def local_submit
+    @course = Course.find(params[:course_id])
+    if !@course then
+      render plain: "ERROR: invalid course", status: :bad_request and return
+    end
+    
+    @user = User.find_by(email: params[:user])
+    @cud = @user and @course.course_user_data.find_by(user_id: @user.id)
+    if !@cud then
+      err = "ERROR: invalid username (#{params[:user]}) for class #{@course.id}"
+      render plain: err, status: :bad_request and return
+    end
+
+    @assessment = @course.assessments.find(params[:id])
+    if !@assessment then
+      err = "ERROR: Invalid Assessment (#{params[:id]}) for course #{@course.id}"
+      render plain: err, status: :bad_request and return
+    elsif @assessment.remote_handin_path.nil? then
+      err = "ERROR: Remote handins have not been enabled by the instructor."
+      render plain: err, status: :bad_request and return
+    end
+
+    personal_directory = @user.email + "_remote_handin_" +  @assessment.name
+    remoteHandinDir = File.join(@assessment.remote_handin_path, personal_directory)
+
+    submission_count = 0
+      
+    if (params[:submit]) then
+      #They've copied their handin over, lets go grab it. 
+      begin
+        handinFile = params[:submit]
+        
+        @cud = @course.course_user_data.find_by(email: @user.email)
+        if @assessment.max_submissions != -1 then
+          submission_count = @cud.submissions.where(assessment: @assessment).size
+          if submission_count >= @assessment.max_submissions then
+            render plain: "You have no remaining submissions for this assessment", status: :bad_request and return
+          end
+        end
+        
+        unless validateForGroups() then
+          render plain: flash[:error], status: :bad_request and return
+        end
+        
+        # save the submissions
+        begin
+          submissions = saveHandin({'local_submit_file'=>File.join(remoteHandinDir, handinFile)})
+        rescue Exception => e
+          submissions = nil
+        end
+        
+        # make sure submission was correctly constructed and saved
+        unless submissions then
+          # Avoid overwriting the flash[:error] set by saveHandin
+          if (!flash[:error].nil? && !flash[:error].empty?) then
+            flash[:error] = "There was an error handing in your submission."
+          end
+          render plain: flash[:error], status: :bad_request and return
+        end
+
+        # autograde the submissions
+        if @assessment.has_autograde then
+          autogradeSubmissions(@course, @assessment, submissions)
+        end
+
+      rescue Exception  => e
+        print e
+        COURSE_LOGGER.log(e.to_s)
+      end
+
+      if (submissions) then
+        puts "Submission received, ID##{submissions[0].id}"
+      else
+        err = "There was an error saving your submission. Please contact your course staff"
+        render plain: err, status: :bad_request and return
+      end
+
+      if @assessment.max_submissions != -1 then
+        render plain: " - You have #{assessment.max_submissions - submissons_count} submissions left" and return
+      end
+    
+      render plain: "Successfully submitted" and return
+    else
+      
+      # Create a handin directory for them. 
+
+      # The handin Directory really should not exist, as this script deletes it
+      # when it's done.  However, if it's there, we'll try to remove an empty
+      # folder, else fail w/ error message. 
+      if (Dir.exist?(remoteHandinDir)) then
+        begin
+          FileUtils.rm_rf(remoteHandinDir)
+        rescue SystemCallError 
+          render plain: "WARNING: could not clear previous handin directory, please" and return
+        end
+      end
+
+      begin
+        Dir.mkdir(remoteHandinDir)
+      rescue SystemCallError
+        puts "ERROR: Could not create handin directory. Please contact
+        autolab-dev@andrew.cmu.edu with this error" 
+      end
+
+      system("fs sa #{remoteHandinDir} #{@user.email} rlidw")
+    end
+
+    render plain: remoteHandinDir and return
+  end
+
+  # method called when student makes
+  # log submission in the database
+  def log_submit
+    @course = Course.find(params[:course_id])
+    if !@course then
+      render plain: "ERROR: invalid course", status: :bad_request and return
+    end
+    
+    @user = User.find_by(email: params[:user])
+    @cud = @user and @course.course_user_data.find_by(user_id: @user.id)
+    if !@cud then
+      err = "ERROR: invalid username (#{params[:user]}) for class #{@course.id}"
+      render plain: err, status: :bad_request and return
+    end
+
+    @assessment = @course.assessments.find(params[:id])
+    if !@assessment then
+      err = "ERROR: Invalid Assessment (#{params[:id]}) for course #{@course.id}"
+      render plain: err, status: :bad_request and return
+    elsif !@assessment.allow_unofficial then
+      err = "ERROR: This assessment does not allow Log Submissions"
+      render plain: err, status: :bad_request and return
+    end
+
+    @result = params[:result]
+    if !@result then
+      render plain: "ERROR: No result!", status: :bad_request and return
+    end
+
+    # Everything looks OK, so append the autoresult to the log.txt file for this lab
+    @logger = Logger.new(Rails.root.join("courses", @course.name, @assessment.name, "log.txt"))
+    @logger.add(Logger::INFO) { "#{@user.email},0,#{@result}" }
+
+    # Load up the lab.rb file
+    modName = @assessment.name + (@course.name).gsub(/[^A-Za-z0-9]/,"")
+    require(Rails.root.join("assessmentConfig", "#{@course.name}-#{@assessment.name}.rb")
+    eval("extend #{modName.camelcase}")
+
+    begin
+      # Call the parseAutoresult function defined in the lab.rb file.  If
+      # the list of scores it returns is empty, then we the lab developer is
+      # asking us not to create an unofficial submission in the
+      # database. Simply return a successful status string to the client and
+      # exit.
+      scores = parseAutoresult(@result,false)
+
+      if scores.keys.length == 0 then 
+        render plain: "OK", status: 200 and return
+      end
+
+      # Try to find an existing submission (always version 0). 
+      submission = @assessment.submissions.find_by(version: 0, course_user_datum_id: @cud.id)
+      if !submission then
+        submission = @assessment.submissions.new(
+              version: 0,
+              autoresult: @result,
+              user_id: @cud.id,
+              submitted_by_id: 0)
+        submission.save!()
+      else
+        #update this one
+        submission.autoresult = @result
+        submission.created_at = Time.now()
+        submission.save!()
+      end
+
+      # Update the scores in the db's unofficial submission using the list
+      # returned by the parseAutoresult function
+      for key in scores.keys do
+        problem = @assessment.problems.find_by(name: key)
+        score = submission.scores.find_or_initialize_by(problem_id: problem.id)
+        score.score = scores[key]
+        score.released = true
+        score.grader_id = 0
+        score.save!()
+      end
+    rescue Exception  => e
+      print e
+    end
+
+    render plain: "OK", status: 200 and return
+  end
+
   private
 
   ##
@@ -105,7 +300,7 @@ module AssessmentHandin
       end
       
       if @assessment.max_submissions != -1 then
-        submission_count = aud.course_user_datum.submissions.size
+        submission_count = aud.course_user_datum.submissions.where(assessment: @assessment).size
         if submission_count >= @assessment.max_submissions then
           flash[:error] = "A member of your group has reached the submission limit for this assessment"
           return false
@@ -119,11 +314,11 @@ module AssessmentHandin
   ##
   # this function returns a list of the submissions created by this handin.
   
-  def saveHandin
+  def saveHandin(sub)
     if !@assessment.has_groups? then
       submission = @assessment.submissions.create(course_user_datum_id: @cud.id,
                                                   submitter_ip: request.remote_ip)
-      submission.saveFile(params[:submission])
+      submission.saveFile(sub)
       return [submission]
     end
       
@@ -132,7 +327,7 @@ module AssessmentHandin
     if group.nil? then
       submission = @assessment.submissions.create(course_user_datum_id: @cud.id,
                                                   submitter_ip: request.remote_ip)
-      submission.saveFile(params[:submission])
+      submission.saveFile(sub)
       return [submission]
     end
     
@@ -141,7 +336,7 @@ module AssessmentHandin
       group.course_user_data.each do |cud|
         submission = @assessment.submissions.create(course_user_datum_id: cud.id,
                                                     submitter_ip: request.remote_ip)
-        submission.saveFile(params[:submission])
+        submission.saveFile(sub)
         submissions << submission
       end
     end
