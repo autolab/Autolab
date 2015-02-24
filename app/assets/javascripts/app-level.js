ce082b852d6b5d5de23e435e306c4e0c52d9b4bc
Daniel Bucci <dlbucci@superftc.com>
2015-02-23 19:53:53 -0500

    Actually Fix Moment DateTimes

--- app/assets/javascripts/app-level.js
+++ app/assets/javascripts/app-level.js
@@ -15,7 +15,7 @@ $(".moment-date-time").each(function(ind) {
 	$el = $(this);
 	var format = $el.data("format")  || "dddd, MMM Do YYYY, h:mm:ss a";
 	var unformattedDate = $el.html();
-	var formattedDate = moment(new Date(unformattedDate), "yy/mm/dd").format(format);
+	var formattedDate = moment(unformattedDate, "YYYY-MM-DD hh:mm:ss ZZ").format(format);
 	$el.html(formattedDate);
 })
 
