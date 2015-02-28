cf3af6df3f1c1a3cd59f6f3ca96bffb80ffd1a39
Ilter Canberk <iltercanberk@gmail.com>
2015-02-28 15:19:55 -0500

    Created the migration that will parse the text

---
+++ db/migrate/20150228200007_split_annotations.rb
@@ -0,0 +1,43 @@
+class SplitAnnotations < ActiveRecord::Migration
+  def change
+
+    Annotation.find_each do |annotation|
+        text = annotation.text
+
+
+        res = text.split("[");
+        
+        if res.size == 1 then
+            annotation.comment = text
+        end
+
+        if res.size > 1 then
+
+            len = res.size
+            points = res[len-1]
+            res.delete_at(len-1)
+
+            points = points.split(":");
+            comment = res.join("[");
+            annotation.comment = comment
+           
+            if points.size == 1 then
+              annotation.value = points[0].delete("]").to_f
+            else
+              if points[0] == "?" then
+                annotation.problem = points[1].delete("]").to_i
+                annotation.value = 0;
+              else
+                problemStr =  points[1].delete("]")
+                problemStr =  problemStr.delete("problem")
+                annotation.problem_id = problemStr.to_i
+                annotation.value =  points[0].to_f
+              end 
+            end
+        end
+
+        annotation.save!
+    end
+
+  end
+end
