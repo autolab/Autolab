d58c5d53f28351c6f2bfad77f65ff60c95d0b4f4
Daniel Bucci <dlbucci@superftc.com>
2015-03-01 14:12:16 -0500

    Added some robustness to migration

--- db/migrate/20150228200007_split_annotations.rb
+++ db/migrate/20150228200007_split_annotations.rb
@@ -1,43 +1,52 @@
 class SplitAnnotations < ActiveRecord::Migration
-  def change
-
+  def up
+    require 'uri'
     Annotation.find_each do |annotation|
-        text = annotation.text
-
-
-        res = text.split("[");
-        
-        if res.size == 1 then
-            annotation.comment = text
+      text = annotation.text
+      puts text
+      if text.blank? then
+        if annotation.destroy then # don't need no blank annotations cluttering up the db
+          puts ">>>>>>>>>>>>>>>>>>>DELETED<<<<<<<<<<<<<<<<<<"
         end
+        next
+      end
 
-        if res.size > 1 then
+      res = text.split("[")
+      
+      comment = text
 
-            len = res.size
-            points = res[len-1]
-            res.delete_at(len-1)
+      if res.size > 1 then
+        len = res.size
+        points = res[len-1]
+        res.delete_at(len-1)
 
-            points = points.split(":");
-            comment = res.join("[");
-            annotation.comment = comment
-           
-            if points.size == 1 then
-              annotation.value = points[0].delete("]").to_f
-            else
-              if points[0] == "?" then
-                annotation.problem = points[1].delete("]").to_i
-                annotation.value = 0;
-              else
-                problemStr =  points[1].delete("]")
-                problem = annotation.submission.assessment.problems.find_by(name: problemStr)
-                annotation.problem = problem
-                annotation.value =  points[0].to_f
-              end 
-            end
+        comment = res.join("[")
+
+        points = points.split(":") 
+        if points.size == 1 then
+          annotation.value = points[0].delete("]").to_f
+        else
+          problemStr =  points[1].delete("]")
+          annotation.problem = annotation.submission.assessment.problems.find_by(name: problemStr)
+          if points[0] == "?" then
+            annotation.value = 0;
+          else
+            annotation.value = points[0].to_f
+          end 
         end
+      end
 
-        annotation.save!
+      annotation.comment = if comment.blank? then
+        "-" # otherwise the validation will fail
+      elsif comment.include? " " then # test for whether this string has been encoded
+        comment
+      else
+        URI.decode(comment)
+      end
+
+      if annotation.save == false then
+        puts "-------------------NOT UPDATED----------------"
+      end
     end
-
   end
 end
