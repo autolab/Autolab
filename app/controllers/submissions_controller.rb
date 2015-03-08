f0823d39ca66ca03efae30411a0ed6b8d1fd2b61
Daniel Bucci <dlbucci@superftc.com>
2015-03-08 14:05:52 -0400

    Viewable PDFs in browser
    If a user tries to view a PDF, the file will be sent to the browser, and
    the browser should display it (tested in FF and Chrome).

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -1,4 +1,5 @@
 require 'archive.rb'
+require 'pdf.rb'
 
 class SubmissionsController < ApplicationController
 
@@ -239,6 +240,11 @@ class SubmissionsController < ApplicationController
       @displayFilename = @submission.filename
     end
     return unless file
+    
+    filename = @submission.handin_file_path
+    if PDF.is_pdf?(file) then
+      send_data(file, type: "application/pdf", disposition: "inline", filename: File.basename(filename)) and return
+    end
     
     begin
       @data = @submission.annotated_file(file, @filename, params[:header_position])
