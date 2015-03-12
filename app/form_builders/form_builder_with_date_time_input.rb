273508dce19655bba083f03a702fba9f4bd034ec
Jacob Zimmerman <zimmerman.jake@gmail.com>
2015-03-11 23:33:53 -0500

    Make datetimepicker support code more robust

--- app/form_builders/form_builder_with_date_time_input.rb
+++ app/form_builders/form_builder_with_date_time_input.rb
@@ -67,8 +67,12 @@ private
   # :greater_than properties to initialize relationships between datepicker
   # fields.
   def date_helper name, options, strftime, date_format
-    existing_time = @object.send(name)
-    formatted_datetime = existing_time.to_time.strftime(strftime) if existing_time.present?
+    begin
+      existing_time = @object.send(name)
+    rescue
+      existing_time = nil
+    end
+    formatted_datetime = existing_time.present? ? existing_time.to_time.strftime(strftime) : ''
 
     field = vanilla_text_field(name, :value => formatted_datetime,
         :class => "form-control datetimepicker",
