1e7b4699fff5229f1cd121ba26706b2478ecc272
Jimmy Zong <yzong@cmu.edu>
2015-03-15 15:09:23 -0400

    Adding Course&Scheduler spec. Refactor helpers.

--- spec/controllers/schedulers_controller_spec.rb
+++ spec/controllers/schedulers_controller_spec.rb
@@ -1,5 +1,205 @@
 require 'rails_helper'
+require 'fileutils'
 
 RSpec.describe SchedulersController, :type => :controller do
 
+  render_views
+
+  before(:suite) {
+    Scheduler.delete_all
+  }
+
+  after(:suite) {
+    Scheduler.delete_all
+  }
+
+  describe "#index" do
+    context "when user is Autolab admin" do
+      u = get_admin
+      login_as(u)
+      cid = get_course_id_by_uid(u.id)
+      it "renders successfully" do
+        get :index, {:course_id => cid}
+        expect(response).to be_success
+        expect(response.body).to match(/Manage Schedulers/m)
+      end
+    end
+
+    context "when user is Autolab instructor" do
+      u = get_instructor
+      login_as(u)
+      cid = get_course_id_by_uid(u.id)
+      it "renders successfully" do
+        get :index, {:course_id => cid}
+        expect(response).to be_success
+        expect(response.body).to match(/Manage Schedulers/m)
+      end
+    end
+
+    context "when user is Autolab user" do
+      u = get_user
+      login_as(u)
+      cid = get_course_id_by_uid(u.id)
+      it "renders with failure" do
+        get :index, {:course_id => cid}
+        expect(response).not_to be_success
+        expect(response.body).not_to match(/Manage Schedulers/m)
+      end
+    end
+
+    context "when user is not logged in" do
+      it "renders with failure" do
+        get :index, {:course_id => 1}
+        expect(response).not_to be_success
+        expect(response.body).not_to match(/Manage Schedulers/m)
+      end
+    end
+  end
+
+  describe "#new" do
+    context "when user is Autolab admin" do
+      u = get_admin
+      login_as(u)
+      cid = get_course_id_by_uid(u.id)
+      it "renders successfully" do
+        get :new, {:course_id => cid}
+        expect(response).to be_success
+        expect(response.body).to match(/New scheduler/m)
+      end
+    end
+
+    context "when user is Autolab instructor" do
+      u = get_instructor
+      login_as(u)
+      cid = get_course_id_by_uid(u.id)
+      it "renders successfully" do
+        get :new, {:course_id => cid}
+        expect(response).to be_success
+        expect(response.body).to match(/New scheduler/m)
+      end
+    end
+
+    context "when user is Autolab user" do
+      u = get_user
+      login_as(u)
+      cid = get_course_id_by_uid(u.id)
+      it "renders with failure" do
+        get :new, {:course_id => cid}
+        expect(response).not_to be_success
+        expect(response.body).not_to match(/New scheduler/m)
+      end
+    end
+
+    context "when user is not logged in" do
+      it "renders with failure" do
+        get :new, {:course_id => 1}
+        expect(response).not_to be_success
+        expect(response.body).not_to match(/New scheduler/m)
+      end
+    end
+  end
+
+  describe "#edit" do
+
+    context "when user is Autolab admin" do
+      u = get_admin
+      login_as(u)
+      cid = get_course_id_by_uid(u.id)
+      s = create_scheduler_with_cid(cid)
+      it "renders successfully" do
+        get :edit, {:course_id => cid, :id => s.id}
+        expect(response).to be_success
+        expect(response.body).to match(/Editing scheduler/m)
+      end
+    end
+
+    context "when user is Autolab instructor" do
+      u = get_instructor
+      login_as(u)
+      cid = get_course_id_by_uid(u.id)
+      s = create_scheduler_with_cid(cid)
+      it "renders successfully" do
+        get :edit, {:course_id => cid, :id => s.id}
+        expect(response).to be_success
+        expect(response.body).to match(/Editing scheduler/m)
+      end
+    end
+
+    context "when user is Autolab user" do
+      u = get_user
+      login_as(u)
+      cid = get_course_id_by_uid(u.id)
+      s = create_scheduler_with_cid(cid)
+      it "renders with failure" do
+        get :edit, {:course_id => cid, :id => s.id}
+        expect(response).not_to be_success
+        expect(response.body).not_to match(/Editing scheduler/m)
+      end
+    end
+
+    context "when user is not logged in" do
+      u = get_admin
+      cid = get_course_id_by_uid(u.id)
+      s = create_scheduler_with_cid(cid)
+      it "renders with failure" do
+        get :edit, {:course_id => 1, :id => s.id}
+        expect(response).not_to be_success
+        expect(response.body).not_to match(/Editing scheduler/m)
+      end
+    end
+  end
+
+  describe "#show" do
+
+    context "when user is Autolab admin" do
+      u = get_admin
+      login_as(u)
+      cid = get_course_id_by_uid(u.id)
+      s = create_scheduler_with_cid(cid)
+      it "renders successfully" do
+        get :show, {:course_id => cid, :id => s.id}
+        expect(response).to be_success
+        expect(response.body).to match(/Action:/m)
+        expect(response.body).to match(/Interval:/m)
+      end
+    end
+
+    context "when user is Autolab instructor" do
+      u = get_instructor
+      login_as(u)
+      cid = get_course_id_by_uid(u.id)
+      s = create_scheduler_with_cid(cid)
+      it "renders successfully" do
+        get :show, {:course_id => cid, :id => s.id}
+        expect(response).to be_success
+        expect(response.body).to match(/Action:/m)
+        expect(response.body).to match(/Interval:/m)
+      end
+    end
+
+    context "when user is Autolab user" do
+      u = get_user
+      login_as(u)
+      cid = get_course_id_by_uid(u.id)
+      s = create_scheduler_with_cid(cid)
+      it "renders with failure" do
+        get :show, {:course_id => cid, :id => s.id}
+        expect(response).not_to be_success
+        expect(response.body).not_to match(/Action:/m)
+        expect(response.body).not_to match(/Interval:/m)
+      end
+    end
+
+    context "when user is not logged in" do
+      u = get_admin
+      cid = get_course_id_by_uid(u.id)
+      s = create_scheduler_with_cid(cid)
+      it "renders with failure" do
+        get :show, {:course_id => 1, :id => s.id}
+        expect(response).not_to be_success
+        expect(response.body).not_to match(/Action:/m)
+        expect(response.body).not_to match(/Interval:/m)
+      end
+    end
+  end
 end
--- spec/support/controller_macros.rb
+++ spec/support/controller_macros.rb
@@ -1,18 +1,62 @@
 module ControllerMacros
+  def get_admin
+    admins = User.where(:administrator => true)
+    return admins.offset(rand(admins.count)).first
+  end
+
+  def get_instructor
+    instructorCUDs = CourseUserDatum.where(:instructor => true)
+    return instructorCUDs.offset(rand(instructorCUDs.count)).first.user
+  end
+
+  def get_course_assistant
+    caCUDs = CourseUserDatum.where(:course_assistant => true)
+    return caCUDs.offset(rand(caCUDs.count)).first.user
+  end
+
+  def get_user
+    users = CourseUserDatum.joins(:user).where({'users.administrator' => false,
+                                                :instructor => 0,
+                                                :course_assistant => 0})
+    return users.offset(rand(users.count)).first.user
+  end
+
   def login_admin
-    before(:each) do
-      @request.env["devise.mapping"] = Devise.mappings[:admin]
-      admins = User.where(:administrator => true)
-      sign_in admins.offset(rand(admins.count)).first
-    end
+    login_as(get_admin)
+  end
+
+  def login_instructor
+    login_as(get_instructor)
+  end
+
+  def login_course_assistant
+    login_as(get_course_assistant)
   end
 
   def login_user
+    login_as(get_user)
+  end
+
+  def login_as(u)
     before(:each) do
       @request.env["devise.mapping"] = Devise.mappings[:user]
-      admins = User.where(:administrator => false)
-      sign_in admins.offset(rand(admins.count)).first
+      sign_in u
     end
   end
-end
 
+  def get_course_id_by_uid(uid)
+    CourseUserDatum.where(:user_id => uid).first.course_id
+  end
+
+  def create_scheduler_with_cid(cid)
+    # Prepare the updater script for scheduler to run
+    update_script_path = Rails.root.join("tmp/testscript.rb");
+    File.open(update_script_path, 'w') { |f|
+      f.write("module Updater def self.update(foo) 0 end end")
+    }
+    s = Course.find(cid).scheduler.new({:action => "tmp/testscript.rb",
+                                        :interval => 86400, :next => Time.now()})
+    s.save
+    return s
+  end
+end
