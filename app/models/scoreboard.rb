ce55e6eba008271affde7fd92d9db0e0d47e564a
Chaskiel Grundman <cg2v@andrew.cmu.edu>
2015-08-24 13:29:03 -0400

    Add some uniqueness checks to models
    Several models should have uniqueness constraints. Missing uniqueness
    constraints can trigger mysql errors when the database has unique keys
    for some columns.

--- app/models/scoreboard.rb
+++ app/models/scoreboard.rb
@@ -7,6 +7,7 @@ class Scoreboard < ActiveRecord::Base
   trim_field :banner, :colspec
 
   validate :colspec_is_well_formed
+  validates_uniqueness_of :assessment_id
 
   after_save -> { assessment.dump_yaml }
 
