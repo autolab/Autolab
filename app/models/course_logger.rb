d1b7933618025ef269970c69466b58bc4269176a
Chaskiel Grundman <cg2v@andrew.cmu.edu>
2016-10-12 18:07:08 -0400

    Update auxiliary loggers to not break main log
    Update COURSE_LOGGER (and add ASSESSMENT_LOGGER) so that the
    custom course and assessment formats are not applies to the
    main rails log. Overriding Logger::format_message prevents certain
    rails features (like log tags) from working.

--- app/models/course_logger.rb
+++ app/models/course_logger.rb
@@ -1,28 +1,50 @@
-class Logger
+
+class CustomLogger
+  def initalize
+    resetPath
+  end
+
+  def setLogPath(path)
+    # if this can't grab the file, Autolab should still function
+    @logger = Logger.new(path, "monthly")
+  rescue
+    @logger = Rails.logger
+  end
+
+  def resetPath
+    @logger = Rails.logger
+  end
+
   def format_message(level, time, _progname, msg)
     strTime = time.strftime("%m/%d/%y %H:%M:%S")
     "#{level} -- #{strTime} -- #{msg}\n"
   end
-end
-
-class CourseLogger
-  def initalize
-    @logger = nil
-  end
-
-  def setCourse(course)
-    # if this can't grab the file, Autolab should still function
-    @logger = Logger.new("#{Rails.root}/courses/#{course.name}/autolab.log", "monthly")
-  rescue
-    @logger = nil
-  end
 
   def log(message, severity = Logger::INFO)
-    if @logger
       @logger.add(severity) { message }
-    else
-      ActionController::Base.logger.add(severity) { message }
-    end
   end
 end
+class CourseLogger < CustomLogger
+  def setCourse(course)
+    setLogPath(Rails.root.join("courses", course.name, "autolab.log"))
+  end
+end
+
+class AssessmentLogger < CustomLogger
+  def initalize
+    @course = nil
+    @assessment = nil
+  end
+  def setCourse(course)
+    @course = course
+    @assessment = nil
+    resetPath
+  end
+  def setAssessment(assessment)
+    @assessment = assessment
+    setLogPath(Rails.root.join("courses", @course.name, @assessment.name, "log.txt"))
+  end
+end
+
 COURSE_LOGGER = CourseLogger.new
+ASSESSMENT_LOGGER = AssessmentLogger.new
