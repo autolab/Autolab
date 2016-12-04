1884bd867f7bc0294b08512a2b4430757da24812
Aatish Nayak <aatishnayak1a@gmail.com>
2016-12-04 02:52:41 -0500

    Convert initial collections and cards

--- app/assets/javascripts/app-level.js
+++ app/assets/javascripts/app-level.js
@@ -1,25 +1,27 @@
 /* JavaScript code that will be added to every page throughout the application */
 
 
-/* Adding CSRF token to every Ajax call by default */
+/* Moment JS generic converter */
+$(document).ready(function() {
+	$(".moment-date-time").each(function(ind) {
+		$el = $(this);
+		var format = $el.data("format")  || "MMMM Do YYYY, h:mm:ss a";
+		var unformattedDate = $el.html();
+		var formattedDate = moment(unformattedDate, "YYYY-MM-DD hh:mm:ss ZZ").format(format);
+		$el.html(formattedDate);
+	});
 
-jQuery.ajaxSetup({
-    headers: {
-        'X-CSRF-Token' : $('meta[name="csrf-token"]').attr('content')
-    },
+	/* Adding CSRF token to every Ajax call by default */
+	jQuery.ajaxSetup({
+    	headers: {
+        	'X-CSRF-Token' : $('meta[name="csrf-token"]').attr('content')
+    	},
+	});
+
+	/* Activating Tooltips */
+	$(function () {
+  		$('[data-toggle="tooltip"]').tooltip()
+	})
 });
 
 
-/* Moment JS generic converter */
-$(".moment-date-time").each(function(ind) {
-	$el = $(this);
-	var format = $el.data("format")  || "dddd, MMM Do YYYY, h:mm:ss a";
-	var unformattedDate = $el.html();
-	var formattedDate = moment(unformattedDate, "YYYY-MM-DD hh:mm:ss ZZ").format(format);
-	$el.html(formattedDate);
-})
-
-/* Activating Tooltips */
-$(function () {
-  $('[data-toggle="tooltip"]').tooltip()
-})
