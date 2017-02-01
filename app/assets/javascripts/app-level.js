e819b869d24b00cd70b3c85271b9cb71d26f1ee8
Billy Zhu <yuxiangz@andrew.cmu.edu>
2017-02-01 10:08:56 -0800

    update default time format

--- app/assets/javascripts/app-level.js
+++ app/assets/javascripts/app-level.js
@@ -5,9 +5,9 @@
 $(document).ready(function() {
 	$(".moment-date-time").each(function(ind) {
 		$el = $(this);
-		var format = $el.data("format")  || "MMMM Do YYYY, HH:mm:ss";
+		var format = $el.data("format")  || "YYYY-MM-DD HH:mm:ss";
 		var unformattedDate = $el.html();
-		var formattedDate = moment(unformattedDate, "YYYY-MM-DD hh:mm ZZ").format(format);
+		var formattedDate = moment(unformattedDate, "YYYY-MM-DD hh:mm:ss ZZ").format(format);
 		$el.html(formattedDate);
 	});
 
