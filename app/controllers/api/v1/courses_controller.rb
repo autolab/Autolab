76aa55c344e2fc097c07983647e5dcad3b5418dd
Billy Zhu <yuxiangz@andrew.cmu.edu>
2017-02-15 17:53:48 -0500

    extend course index method with optional state param

--- app/controllers/api/v1/courses_controller.rb
+++ app/controllers/api/v1/courses_controller.rb
@@ -3,6 +3,18 @@ class Api::V1::CoursesController < Api::V1::BaseApiController
   def index
     courses_for_user = User.courses_for_user current_user
 
+    if params.has_key?(:state)
+    	if params[:state] == "disabled"
+    		courses_for_user = courses_for_user.select { |course| course.disabled? }
+    	elsif ["completed", "current", "upcoming"].include? params[:state]
+    		state = params[:state].to_sym
+    		courses_for_user = courses_for_user.select { |course| course.temporal_status == state }
+    	else
+    		# invalid state
+    		raise ApiError.new("Unexpected course state", :bad_request)
+    	end
+    end
+
     respond_with courses_for_user
   end
 
