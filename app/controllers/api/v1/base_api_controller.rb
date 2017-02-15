f17f500360f55e280776a1be137d36710370b0c0
Billy Zhu <yuxiangz@andrew.cmu.edu>
2017-02-15 17:51:35 -0500

    implement api error handling

--- app/controllers/api/v1/base_api_controller.rb
+++ app/controllers/api/v1/base_api_controller.rb
@@ -1,5 +1,17 @@
 class Api::V1::BaseApiController < ActionController::Base
 
+  # Error type for APIs
+  class ApiError < StandardError
+    attr_reader :status_code # http error code
+    
+    def initialize(msg = "Unrecognized request", status_code = :bad_request)
+      @status_code = status_code
+      super(msg)
+    end
+  end
+
+  rescue_from ApiError, with: :respond_with_api_error
+
   before_action :doorkeeper_authorize! # OAuth2 token authentication for all actions
 
   respond_to :json
@@ -8,6 +20,10 @@ class Api::V1::BaseApiController < ActionController::Base
 
   def current_user
     @current_user ||= User.find(doorkeeper_token[:resource_owner_id])
+  end
+
+  def respond_with_api_error(error)
+    render :json => {:error => error.message}.to_json, :status => error.status_code
   end
 
 end
