beb55d6cba32a4fa11102bce08a1f1b5b57f6d9e
Billy Zhu <yuxiangz@andrew.cmu.edu>
2017-04-05 17:59:36 -0400

    add failure cases to course test

--- spec/api/v1/courses_api_spec.rb
+++ spec/api/v1/courses_api_spec.rb
@@ -6,6 +6,11 @@ RSpec.describe Api::V1::CoursesController, :type => :controller do
     let!(:application) { Doorkeeper::Application.create! :name => "TestApp", :redirect_uri => "https://example.com" }
     let!(:token) { Doorkeeper::AccessToken.create! :application_id => application.id, :resource_owner_id => user.id }
 
+    it 'fails to authenticate' do
+      get :index, :access_token => token.token.length.times.map { (65 + rand(26)).chr }.join # a random token
+      expect(response.response_code).to eq(401)
+    end
+
     it 'returns all the user\'s courses' do
       get :index, :access_token => token.token
       expect(response.response_code).to eq(200)
@@ -20,6 +25,13 @@ RSpec.describe Api::V1::CoursesController, :type => :controller do
         course = Course.find(:name => c["name"])
         expect{course.temporal_status}.to eq(:current)
       end
+    end
+
+    it 'reports a state error' do
+      get :index, :access_token => token.token, :state => "blah"
+      expect(response.response_code).to eq(400)
+      msg = JSON.parse(response.body)
+      expect(msg["error"]).to eq("Unexpected course state")
     end
   end
 end
