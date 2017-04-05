8f908ac0af8329213bdfcd32ae251c10e80cec88
Billy Zhu <yuxiangz@andrew.cmu.edu>
2017-04-05 17:00:39 -0400

    add course index test

--- spec/api/v1/courses_api_spec.rb
+++ spec/api/v1/courses_api_spec.rb
@@ -1,8 +1,25 @@
 require 'rails_helper'
 
-RSpec.describe "Courses API", :type => :request do
-	it "returns list of user courses" do
-		
-	end
+RSpec.describe Api::V1::CoursesController, :type => :controller do
+	describe 'GET #index' do
+    user = get_user
+    let!(:application) { Doorkeeper::Application.create! :name => "TestApp", :redirect_uri => "https://example.com" }
+    let!(:token) { Doorkeeper::AccessToken.create! :application_id => application.id, :resource_owner_id => user.id }
 
+    it 'returns all the user\'s courses' do
+      get :index, :access_token => token.token
+      expect(response.response_code).to eq(200)
+      expect{ msg = JSON.parse(response.body) }.not_to raise_exception
+    end
+
+    it 'returns only current courses' do
+      get :index, :access_token => token.token, :state => "current"
+      expect(response.response_code).to eq(200)
+      msg = JSON.parse(response.body)
+      msg.each do |c|
+        course = Course.find(:name => c["name"])
+        expect{course.temporal_status}.to eq(:current)
+      end
+    end
+  end
 end
