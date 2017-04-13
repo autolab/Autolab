e297b5d2833905cd9c0025f3bfe95f1a45760cdc
Billy Zhu <yuxiangz@andrew.cmu.edu>
2017-04-13 00:55:22 -0400

    throttle using user id instead of ip

--- config/initializers/rack-attack.rb
+++ config/initializers/rack-attack.rb
@@ -1,4 +1,17 @@
 class Rack::Attack
+  class Request < ::Rack::Request
+    # get user id from access_token
+    # Note: access_token should be present. If not present, request is not throttled.
+    def user_id
+      return env["attack.user_id"] if env["attack.user_id"]
+
+      token = params['access_token']
+      access_token = Doorkeeper::AccessToken.where(token: token).first
+      user = User.find(access_token.resource_owner_id)
+      env["attack.user_id"] = user.id
+      return user.id
+    end
+  end
 
   ### Configure Cache ###
 
@@ -11,26 +24,28 @@ class Rack::Attack
 
   # Rack::Attack.cache.store = ActiveSupport::Cache::MemoryStore.new 
 
-  ### Safelist any request not to API ###
-  Rack::Attack.safelist('allow from localhost') do |req|
-    not req.path.start_with?("/api/")
+  ### Safelist Requests ###
+
+  # Safelist any request not to API or does not have access_token
+  safelist('allow from localhost') do |req|
+    (not req.path.start_with?("/api/")) || (not req.params['access_token'])
   end
 
-  ### Throttle General API Requests ###
+  ### Throttle Requests ###
 
-  # Throttle all requests by IP address
+  # Throttle all requests by user id
   #
-  # Key: "rack::attack:#{Time.now.to_i/:period}:api/general:#{req.ip}"
+  # Key: "rack::attack:#{Time.now.to_i/:period}:api/general:#{req.user_id}"
   throttle('api/general', :limit => 10, :period => 30.seconds) do |req|
-    req.ip
+    req.user_id
   end
 
   # Throttle requests for assessment submission endpoint
   #
-  # Key: "rack::attack:#{Time.now.to_i/:period}:api/submit:#{req.ip}"
+  # Key: "rack::attack:#{Time.now.to_i/:period}:api/submit:#{req.user_id}"
   throttle("api/submit", :limit => 4, :period => 1.minute) do |req|
     if req.path.end_with?("submit")
-      req.ip
+      req.user_id
     end
   end
 
