ced57e529f91e00c85bbfbc5191ef15e720fb1f9
Aatish Nayak <aatishn@andrew.cmu.edu>
2017-04-16 13:07:25 -0400

    Set default date if val is blank (#855)

--- app/assets/javascripts/initialize_datetimepickers.js
+++ app/assets/javascripts/initialize_datetimepickers.js
@@ -3,20 +3,31 @@
   
   $(document).ready(function() {
     var datetimeElts = $('.datetimepicker');
+
     for (var i = 0; i < datetimeElts.length; i++) {
+      var defaultDate = new Date(moment())
+      if ($(datetimeElts[i]).val()) {
+        defaultDate = new Date(moment($(datetimeElts[i]).val()))
+      }
+
       $(datetimeElts[i]).flatpickr({
         enableTime: true,
         altInput: true,
-        defaultDate: new Date(moment($(datetimeElts[i]).val()))
+        defaultDate: defaultDate
       })
     }
 
     var dateElts = $('.datepicker');
 
     for (var i = 0; i < dateElts.length; i++) {
+      var defaultDate = new Date(moment())
+      if ($(datetimeElts[i]).val()) {
+        defaultDate = new Date(moment($(datetimeElts[i]).val()).format("MMMM D YYYY"))
+      }
+
       $(dateElts[i]).flatpickr({
         altInput: true,
-        defaultDate: new Date(moment($(dateElts[i]).val()).format("MMMM D YYYY"))
+        defaultDate: defaultDate
       })
     }
   });
