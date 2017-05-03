507ee8e18bdb18be965c4bdc10ba3e2880f6b0d4
Billy Zhu <yuxiangz@andrew.cmu.edu>
2017-05-03 17:50:46 -0400

    add assessment#submit endpoint tests

--- spec/api/v1/assessments_api_spec.rb
+++ spec/api/v1/assessments_api_spec.rb
@@ -38,4 +38,61 @@ RSpec.describe Api::V1::AssessmentsController, :type => :controller do
       expect(msg.length).to eq(assessment.problems.count)
     end
   end
+
+  describe 'POST #submit' do
+    # Note: this test context does not use shared context
+    before :context do
+      # The adder.py file to hand in
+      @handin_file = fixture_file_upload('handins/adder.py', 'text/plain')
+      # The AutoPopulate Course
+      @ap_course = Course.find_by(:name => 'AutoPopulated')
+      @ap_cud = CourseUserDatum.where(:course => @ap_course, :instructor => false, :course_assistant => false).first
+      @ap_student = @ap_cud.user
+      # The adder.py Assessment
+      @adder_asm = Assessment.find_by(:course => @ap_course, :name => 'labtemplate')
+    end
+
+    let!(:bad_application) { Doorkeeper::Application.create! :name => "TestApp", :redirect_uri => "https://example.com", :scopes => "user_info user_courses" }
+    let!(:bad_token) { Doorkeeper::AccessToken.create! :application_id => application.id, :resource_owner_id => @ap_student.id, :scopes => "user_info user_courses" }
+
+    let!(:application) { Doorkeeper::Application.create! :name => "TestApp", :redirect_uri => "https://example.com", :scopes => "user_info user_submit" }
+    let!(:token) { Doorkeeper::AccessToken.create! :application_id => application.id, :resource_owner_id => @ap_student.id, :scopes => "user_info user_submit" }
+
+    it 'fails scope test' do
+      subm = Hash.new
+      subm['file'] = @handin_file
+      post :submit, :access_token => bad_token.token, :course_name => @ap_course.name, :assessment_name => @adder_asm.name, :submission => subm
+      expect(response.response_code).to eq(403)
+      msg = JSON.parse(response.body)
+      expect(msg).to include('error')
+      expect(msg['error']).to include('scope')
+    end
+
+    it 'rejects invalid handin with no submission[file] param' do
+      subm = Hash.new
+      post :submit, :access_token => token.token, :course_name => @ap_course.name, :assessment_name => @adder_asm.name, :submission => subm
+      expect(response.response_code).to eq(400)
+      msg = JSON.parse(response.body)
+      expect(msg).to include('error')
+      expect(msg['error']).to include('parameter')
+    end
+
+    it 'accepts the handin' do
+      # count number of submissions before
+      sub_count = Submission.where(:course_user_datum_id => @ap_cud.id, :assessment => @adder_asm).count
+
+      # perform submission
+      subm = Hash.new
+      subm['file'] = @handin_file
+      post :submit, :access_token => token.token, :course_name => @ap_course.name, :assessment_name => @adder_asm.name, :submission => subm
+      msg = JSON.parse(response.body)
+      expect(response.response_code).to eq(200)
+      expect(msg).to include('success')
+      expect(msg['success']).to match(/Submitted file [^\s]+ for autograding/)
+
+      # count submissions after
+      sub_count_after = Submission.where(:course_user_datum_id => @ap_cud.id, :assessment => @adder_asm).count
+      expect(sub_count_after - 1).to eq(sub_count)
+    end
+  end
 end
