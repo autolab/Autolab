391365cd4fa9eb854a595391dd85593ea9eb50e1
devanshk <devanshkukreja1@gmail.com>
2017-09-18 23:01:42 -0400

    fixed annotation deletion also deleting score bug and added a note for instructors

--- db/migrate/20131222162700_prepare_external_auth.rb
+++ db/migrate/20131222162700_prepare_external_auth.rb
@@ -2,7 +2,7 @@ class PrepareExternalAuth < ActiveRecord::Migration
   def up
     # rename old :users table to :course_user_data table
     rename_table :users, :course_user_data
-    
+
     # create/recover new :users table
     if table_exists? :users_backup then
       rename_table :users_backup, :users
@@ -42,13 +42,13 @@ class PrepareExternalAuth < ActiveRecord::Migration
 
         t.timestamps
       end
-    
+
       add_index :users, :email,                :unique => true
       add_index :users, :reset_password_token, :unique => true
       add_index :users, :confirmation_token,   :unique => true
       # add_index :users, :unlock_token,         :unique => true
     end
-    
+
     # create/recover :authentications table
     if table_exists? :authentications_backup then
       rename_table :authentications_backup, :authentications
@@ -61,12 +61,12 @@ class PrepareExternalAuth < ActiveRecord::Migration
         t.timestamps
       end
     end
-    
+
     # build relation between :users and :course_user_data
     if column_exists? :course_user_data, :user_id_backup then
       rename_column :course_user_data, :user_id_backup, :user_id
     else
-      add_column :course_user_data, :user_id, :integer, null: false
+      add_column :course_user_data, :user_id, :integer, null: false, default: 0
     end
 
     rename_column :course_user_data, :first_name, :first_name_backup
@@ -80,14 +80,14 @@ class PrepareExternalAuth < ActiveRecord::Migration
     CourseUserDatum.find_each do |cud|
       if cud.user.nil? then # no user field
         user = User.where(:email => cud.email_backup).first
-        
+
         if user.nil? then # user haven't been created yet
           # build "CMU-Shibboleth" authentication object
           auth = Authentication.new
           auth.provider = "CMU-Shibboleth"
           auth.uid = cud.andrewID_backup + "@andrew.cmu.edu"
           auth.save!
-          
+
           # build user object
           user = User.new
           user.first_name = cud.first_name_backup
@@ -95,12 +95,12 @@ class PrepareExternalAuth < ActiveRecord::Migration
           user.email = cud.email_backup
           user.administrator = cud.administrator_backup
           user.authentications << auth
-        
+
           temp_pass = Devise.friendly_token[0,20] # use a random token
           user.password = temp_pass
           user.password_confirmation = temp_pass
           user.skip_confirmation!
-        
+
           user.save!
         end
         cud.user = user
@@ -110,21 +110,21 @@ class PrepareExternalAuth < ActiveRecord::Migration
       end
     end
 
-    # change user reference from other models    
+    # change user reference from other models
     rename_column :announcements, :user_id, :course_user_datum_id
     # remove this index. Otherwise, Rails will err on long index name
     if index_exists?(:assessment_user_data, ["user_id", "assessment_id"])
       remove_index :assessment_user_data, ["user_id", "assessment_id"]
     end
-    rename_column :assessment_user_data, :user_id, 
+    rename_column :assessment_user_data, :user_id,
           :course_user_datum_id
     add_index :assessment_user_data, ["course_user_datum_id", "assessment_id"],
               :name => "index_AUDs_on_CUD_id_and_assessment_id"
     rename_column :extensions, :user_id, :course_user_datum_id
     rename_column :submissions, :user_id, :course_user_datum_id
-    
+
   end
-  
+
   def down
     # recover columns of :course_user_data table
     rename_column :course_user_data, :first_name_backup, :first_name
@@ -133,19 +133,19 @@ class PrepareExternalAuth < ActiveRecord::Migration
     change_column :course_user_data, :andrewID, :string, :default => nil
     rename_column :course_user_data, :email_backup, :email
     rename_column :course_user_data, :administrator_backup, :administrator
-    
+
     # backup cud user_id
     rename_column :course_user_data, :user_id, :user_id_backup
-    
+
     # backup :users table
     rename_table :users, :users_backup
-    
+
     # backup :authentications table
     rename_table :authentications, :authentications_backup
-    
+
     # rename :course_user_data table to :users table
     rename_table :course_user_data, :users
-    
+
     # change user reference from other models
     rename_column :announcements, :course_user_datum_id, :user_id
     remove_index :assessment_user_data, :name => "index_AUDs_on_CUD_id_and_assessment_id"
@@ -154,7 +154,7 @@ class PrepareExternalAuth < ActiveRecord::Migration
     add_index "assessment_user_data", ["user_id", "assessment_id"]
     rename_column :extensions, :course_user_datum_id, :user_id
     rename_column :submissions, :course_user_datum_id, :user_id
-    
+
   end
-  
+
 end
