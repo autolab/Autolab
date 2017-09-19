15e5613c8831c408bf8ac183594fd18f5c2e0ab6
devanshk <devanshkukreja1@gmail.com>
2017-09-18 23:04:44 -0400

    reverted db migration changes

--- db/migrate/20130330220843_add_grading_deadline_to_assessment.rb
+++ db/migrate/20130330220843_add_grading_deadline_to_assessment.rb
@@ -1,6 +1,6 @@
 class AddGradingDeadlineToAssessment < ActiveRecord::Migration
   def self.up
-    add_column :assessments, :grading_deadline, :datetime, :null => false, :default => Time.now
+    add_column :assessments, :grading_deadline, :datetime, :null => false
     Assessment.find_each do |asmt|
       asmt.update_attribute(:grading_deadline, asmt.submit_until)
     end
