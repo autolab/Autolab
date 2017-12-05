63e8affec826313b7658699e11f433256a31a7ef
devanshk <devanshkukreja1@gmail.com>
2017-12-05 12:31:39 -0500

    decoupled annotations and gradebook again

--- app/controllers/annotations_controller.rb
+++ app/controllers/annotations_controller.rb
@@ -19,28 +19,6 @@ class AnnotationsController < ApplicationController
   def create
     annotation = @submission.annotations.new(annotation_params)
 
-    if !annotation_params[:problem_id].blank?
-      findScore = Score.where('submission_id = ? AND problem_id = ?', params[:submission_id] , annotation_params[:problem_id])
-    else
-      annotation.save
-      respond_with(@course, @assessment, @submission, annotation)
-      return
-    end
-
-    if findScore.blank?
-      score = Score.new
-      score.submission_id =  params[:submission_id]
-      score.score = annotation_params[:value]
-      score.problem_id = annotation_params[:problem_id]
-      score.released = 0
-      score.grader_id = @cud.id
-      score.save
-    else
-      findScore.first.score += annotation_params[:value].to_f
-      findScore.first.grader_id = @cud.id
-      findScore.first.save
-    end
-
     annotation.save
     respond_with(@course, @assessment, @submission, annotation)
   end
@@ -48,49 +26,6 @@ class AnnotationsController < ApplicationController
   # PUT /:course/annotations/1.json
   action_auth_level :update, :course_assistant
   def update
-
-    if !annotation_params[:problem_id].blank?
-      findScore = Score.where('submission_id = ? AND problem_id = ?', params[:submission_id] , annotation_params[:problem_id])
-    else
-      if annotation_params[:value].to_f != 0
-        findScore = Score.where('submission_id = ? AND problem_id = ?', params[:submission_id] , annotation_params[:problem_id])
-      else
-        @annotation.save
-        respond_with(@course, @assessment, @submission, @annotation)
-      end
-    end
-
-
-
-    if !@annotation.problem_id.blank?
-      oldScore = Score.where('submission_id = ? AND problem_id = ?', params[:submission_id] , @annotation.problem_id)
-    end
-
-    if findScore.blank?
-      score = Score.new
-      score.submission_id =  params[:submission_id]
-      score.score = annotation_params[:value]
-      score.problem_id = annotation_params[:problem_id]
-      score.released = 0
-      score.grader_id = @cud.id
-      score.save
-    else
-      # If we didn't change the problem number, nothing to worry about
-      if (annotation_params[:problem_id] == @annotation.problem_id)
-        findScore.first.score -= @annotation.value.to_f
-        findScore.first.score += annotation_params[:value].to_f
-        findScore.first.grader_id = @cud.id
-        findScore.first.save
-      # But if we did, we need to credit the points back to the original problem
-      else
-        findScore.first.score += annotation_params[:value].to_f
-        findScore.first.grader_id = @cud.id
-        findScore.first.save
-        oldScore.first.score -= @annotation.value.to_f
-        oldScore.first.grader_id = @cud.id
-        oldScore.first.save
-      end
-    end
     @annotation.update(annotation_params)
     respond_with(@course, @assessment, @submission, @annotation) do |format|
       format.json { render json: @annotation }
@@ -100,14 +35,8 @@ class AnnotationsController < ApplicationController
   # DELETE /:course/annotations/1.json
   action_auth_level :destroy, :course_assistant
   def destroy
-    # remove score entry and delete annoation
-    if !@annotation.problem_id.blank?
-      findScore = Score.where('submission_id = ? AND problem_id = ?', @annotation.submission_id , @annotation.problem_id)
-      findScore.first.score -= @annotation.value
-      findScore.first.save
-    end
+    
     @annotation.destroy
-    head :no_content
   end
 
 private
