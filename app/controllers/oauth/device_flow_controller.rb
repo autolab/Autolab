d6c621f1aba31bd3ec18360254cd3f853d87eb66
Billy Zhu <yuxiangz@andrew.cmu.edu>
2017-12-25 22:43:14 -0500

    implement device_flow_init

--- app/controllers/oauth/device_flow_controller.rb
+++ app/controllers/oauth/device_flow_controller.rb
@@ -1,11 +1,29 @@
 class Oauth::DeviceFlowController < ActionController::Base
+  # expects params: client_id
   def init
-    puts "INIT"
-    render :json => {:msg => "got init"}.to_json
+    if not params.has_key?(:client_id)
+      render json: {:error => "Required param 'client_id' not present"}, status: :bad_request
+      return
+    end
+
+    app = Doorkeeper::Application.find_by(uid: params[:client_id])
+    if app.nil?
+      render json: {:error => "Unrecongized client_id"}, status: :bad_request
+      return
+    end
+
+    req = OauthDeviceFlowRequest.create_request(app)
+    if req.nil?
+      render json: {:error => "Failed to create request, try again later"}, status: :internal_server_error
+      return
+    end
+
+    # success!
+    render json: {device_code: req.device_code, user_code: req.user_code}, status: :ok
   end
 
   def authorize
     puts "AUTHORIZE"
-    render :json => {:msg => "got authorize"}.to_json
+    render json: {:msg => "got authorize"}
   end
 end
