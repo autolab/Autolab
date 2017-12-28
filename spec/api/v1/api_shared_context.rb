53934abd47bbec0750459a7c139d7437f1e17f78
Billy Zhu <yuxiangz@andrew.cmu.edu>
2017-12-28 02:29:35 -0500

    implement tests for device_flow api endpoints

--- spec/api/v1/api_shared_context.rb
+++ spec/api/v1/api_shared_context.rb
@@ -3,6 +3,7 @@ RSpec.shared_context "api shared context" do
   let(:user) { all_users.offset(rand(all_users.count)).first.user }
   let(:course) { CourseUserDatum.where(user_id: user.id).first.course }
   let(:assessment) { course.assessments.offset(rand(course.assessments.count)).first }
+  let(:msg) { JSON.parse(response.body) }
 
   # default application with access to user_info and user_courses
   let!(:application) { Doorkeeper::Application.create! :name => "TestApp", :redirect_uri => "https://example.com", :scopes => "user_info user_courses" }
@@ -11,4 +12,8 @@ RSpec.shared_context "api shared context" do
   # user_info app
   let!(:user_info_application) { Doorkeeper::Application.create! :name => "UserInfoApp", :redirect_uri => "https://user-info-example.com", :scopes => "user_info" }
   let!(:user_info_token) { Doorkeeper::AccessToken.create! :application_id => user_info_application.id, :resource_owner_id => user.id, :scopes => "user_info" }
+
+  # device_flow app
+  # The redirect_uri is just a stub. It is not involved in the test.
+  let!(:df_application) { Doorkeeper::Application.create! :name => "CLIApp", :redirect_uri => "https://localhost:3000/device_flow_auth_cb", :scopes => "user_info user_courses" }
 end
