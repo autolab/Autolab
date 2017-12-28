cad4c0100882edd9c1b21c3318eed6681a3bd3b8
Billy Zhu <yuxiangz@andrew.cmu.edu>
2017-12-28 05:20:54 -0500

    update roundtrip test to test for submitted_by_app_id attribute

--- spec/api/v1/submission_roundtrip_spec.rb
+++ spec/api/v1/submission_roundtrip_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe "API Submission Autograding Roundtrip Test", :type => :request do
   # Note: this test context does not use shared context
-  before :context do
+  before :each do
     # The adder.py file to hand in
     @handin_file = fixture_file_upload('handins/adder.py', 'text/plain')
     # The AutoPopulate Course
@@ -11,6 +11,10 @@ RSpec.describe "API Submission Autograding Roundtrip Test", :type => :request do
     @ap_student = @ap_cud.user
     # The adder.py Assessment
     @adder_asm = Assessment.find_by(:course => @ap_course, :name => 'labtemplate')
+    # make sure we can submit to this assessment
+    @adder_asm.due_at = Time.now + 1.hour
+    @adder_asm.end_at = Time.now + 1.hour
+    @adder_asm.save!
   end
   
   let!(:application) { Doorkeeper::Application.create! :name => "TestApp", :redirect_uri => "https://example.com", :scopes => "user_info user_submit" }
@@ -33,8 +37,11 @@ RSpec.describe "API Submission Autograding Roundtrip Test", :type => :request do
     sub_count_after = Submission.where(:course_user_datum_id => @ap_cud.id, :assessment => @adder_asm).count
     expect(sub_count_after - 1).to eq(sub_count)
 
-    # mock a Tango callback
+    # check submitted_by_app_id
     latest_sub = Submission.where(:course_user_datum_id => @ap_cud.id, :assessment => @adder_asm).order(:version).last
+    expect(latest_sub.submitted_by_app_id).to eq(application.id)
+
+    # mock a Tango callback
     feedback_file = fixture_file_upload('feedback.txt', 'text/plain')
     post "/courses/#{@ap_course.name}/assessments/#{@adder_asm.name}/autograde_done?dave=#{latest_sub.dave}&submission_id=#{latest_sub.id}", :file => feedback_file
 
