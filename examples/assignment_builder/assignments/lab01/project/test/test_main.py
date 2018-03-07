7db93995d77c8a6c3164900d490f37484af8f089
Kolowitz <bkolowitz@gmail.com>
2018-03-07 12:12:28 -0500

    remove unnecessary tests

--- examples/assignment_builder/assignments/lab01/project/test/test_main.py
+++ examples/assignment_builder/assignments/lab01/project/test/test_main.py
@@ -13,21 +13,3 @@ def test_get_age():
     expected = 30
     actual = main.get_age()
     assert expected == actual
-
-
-def test_get_python_version():
-    expected = sys.version
-    actual = main.get_python_version()
-    assert expected == actual
-
-
-def test_get_greeting():
-    expected = "It's nice to meet everyone."
-    actual = main.get_greeting()
-    assert expected == actual
-
-
-def test_main():
-    expected = 5
-    actual = len(main.main())
-    assert expected == actual
