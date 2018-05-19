4c43aa3b00c7914da48f0f0e459ee655e6511113
devanshk <devanshkukreja1@gmail.com>
2018-05-19 02:24:45 -0700

    null constraint workaround for sql

--- db/migrate/20130330220843_add_grading_deadline_to_assessment.rb
+++ db/migrate/20130330220843_add_grading_deadline_to_assessment.rb
@@ -1,6 +1,7 @@
 class AddGradingDeadlineToAssessment < ActiveRecord::Migration
   def self.up
-    add_column :assessments, :grading_deadline, :datetime, :null => false
+    add_column :assessments, :grading_deadline, :datetime
+    change_column :assessments, :grading_deadline, :datetime, :null => false
     Assessment.find_each do |asmt|
       asmt.update_attribute(:grading_deadline, asmt.submit_until)
     end
