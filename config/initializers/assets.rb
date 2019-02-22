acea3ce2c7f5cea8177945d740d428f6c2d1e979
Zeng Fan Pu <zengfanpu@gmail.com>
2019-02-21 19:09:24 -0500

    Precompile navbar.css

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,2 +1,3 @@
 Rails.application.config.assets.precompile += %w( animate.css )
 Rails.application.config.assets.precompile += %w( style.css )
+Rails.application.config.assets.precompile += %w( navbar.css )
