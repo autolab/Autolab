43759f6f04c174ed6d916b8e6522b4b711ebeb81
devanshk <devanshkukreja1@gmail.com>
2019-03-30 13:08:22 -0400

    fixed precompile bug

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,5 +9,4 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
-Rails.application.config.assets.precompile += %w( *.js *.css )
-
+Rails.application.config.assets.precompile += %w( *.js animate.css style.css navbar.css)
