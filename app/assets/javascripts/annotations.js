cfd09ac4d8fa1dab42a70126d7c7c9c888b01d89
devanshk <devanshkukreja1@gmail.com>
2019-04-04 18:12:54 -0400

    added grade updating to speedgrader

--- app/assets/javascripts/annotations.js
+++ app/assets/javascripts/annotations.js
@@ -74,6 +74,84 @@ $(document).keydown(function(e) {
     e.preventDefault(); // prevent the default action (scroll / move caret)
 });
 
+// Make the grades in the mini tab editable
+function make_editable($editable) {
+  // click/enter to edit cells
+
+  /* Calls the Jquery plugin Jeditable to set up the element for in-place editing.
+   * When done editing, a request to quickSetScore (on server) is called with the 
+   * following parameters.
+   * 
+   * Note: Expects editableUrl to be set in javascript already and map to
+   * something like url_for ([:quickSetScore, @course, @assessment])
+   */
+  $editable.editable(editableUrl, {
+      name: 'score',
+      event: 'click',
+      placeholder: "&ndash;",
+      select: true, // select all text in score editor on click/enter
+      onblur: function() {
+        console.log("grade change onBlur")
+      },
+      onreset: function(event) {
+        console.log("grade change onReset")
+      },
+      onerror: function() {
+        // TODO: Display a message on save error
+        console.log("grade change onError")
+      },
+      onsubmit: function() {
+          // TODO: Don't submit a score chance if we have it in the cache
+          console.log("grade change onSubmit")
+
+          // // check if score changed (submit to server only if diff from cache)
+          // var sub_id = editor.data("submission-id");
+          // var prob_id = editor.data("problem-id");
+
+          // var old_score = cache_get_item(sub_id, prob_id, 'score');
+          // var curr_score = $('input',this).val();
+          // if (curr_score != old_score) {
+          //   // update cache
+          //   cache_insert(sub_id, prob_id, 'score', curr_score);
+          //   console.log("submitting changes to score");
+          //   return true;
+          // }
+          // close_current_editor();
+          return true;
+      },
+      submitdata: function(value, settings) {
+        console.log("grade change submitData")
+        requestData = {
+          submission_id: $editable.data('submission-id'),
+          problem_id: $editable.data('problem-id')
+        }
+        console.log("data is: ", requestData)
+        return requestData
+      },
+      callback: function(value, settings) {
+          // TODO: Display a success message
+          console.log("grade change callback")
+
+          // jQuery.ajax("quickGetTotal", {
+          //     data: {
+          //         submission_id: editor.data('submission-id')
+          //     }, 
+          //     success: function(data, status, jqXHR) {
+          //         // TODO: wtf
+          //         total.html(data == " " ? "&ndash;" : data);
+          //         total.effect("highlight", {}, 1000);
+          //     },
+          //     error: function() {
+          //         total.text("?");
+          //     }
+          // });
+
+      }
+
+  });
+
+}
+
 
 /* Highlights lines longer than 80 characters autolab red color */
 var highlightLines = function(highlight) {
