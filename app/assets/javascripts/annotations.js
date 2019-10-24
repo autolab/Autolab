e00a8fc796e38ce4c64515aea8871148911e8658
Zeng Fan Pu <zengfanpu@gmail.com>
2019-10-24 15:55:53 -0400

    Change usage of let to var

--- app/assets/javascripts/annotations.js
+++ app/assets/javascripts/annotations.js
@@ -205,13 +205,13 @@ $(document).keydown(function(e) {
 
 /* Some Helper functions */
 function copyToClipboard(str) {
-  let el = document.createElement('textarea'); // Create a <textarea> element
+  var el = document.createElement('textarea'); // Create a <textarea> element
   el.value = str; // Set its value to the string that you want copied
   el.setAttribute('readonly', ''); // Make it readonly to be tamper-proof
   el.style.position = 'absolute';
   el.style.left = '-9999px'; // Move outside the screen to make it invisible
   document.body.appendChild(el); // Append the <textarea> element to the HTML document
-  let selected =
+  var selected =
     document.getSelection().rangeCount > 0 // Check if there is any content selected previously
     ?
     document.getSelection().getRangeAt(0) // Store selection if found
