511af9873d267e624608b850aeef6d3cd953c590
Li Yicheng <liyicheng340@gmail.com>
2019-11-17 23:25:10 -0500

    Set is_autograded=true in saveAutograde() function

--- app/helpers/assessment_autograde_core.rb
+++ app/helpers/assessment_autograde_core.rb
@@ -353,6 +353,7 @@ module AssessmentAutogradeCore
           score.feedback = lines.join
           score.released = @autograde_prop.release_score
           score.grader_id = 0
+          score.is_autograded = true
           score.save!
         end
       end
