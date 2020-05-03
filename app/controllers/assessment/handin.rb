24e739ef2fde007382f4d1170b2ca190944b8df1
Oliver Li <liyicheng340@gmail.com>
2020-05-03 10:19:01 -0400

    Check for malformed handins early (#1138)
    * Check for malformed handins early
    
    See https://github.com/cg2v/Autolab/commit/df5e132d63eb25dade8911329ffb2cc777da4baa
    
    * changed tabs to spaces
    
    Co-authored-by: Fan Pu <zengfanpu@gmail.com>

--- app/controllers/assessment/handin.rb
+++ app/controllers/assessment/handin.rb
@@ -301,8 +301,16 @@ private
   # submission file is okay to submit.
   #
   def validateHandin_forHTML
+    if params[:submission].blank?
+        flash[:error] = "Submission was blank - please upload again."
+        return false
+    end
+    if params[:submission]["file"].blank?
+        flash[:error] = "Submission was blank (file upload missing) - please upload again."
+        return false
+    end
     # check for custom form first
-		if @assessment.has_custom_form
+    if @assessment.has_custom_form
       for i in 0..@assessment.getTextfields.size-1
           if params[:submission][("formfield" + (i+1).to_s).to_sym].blank?
             flash[:error] = @assessment.getTextfields[i] + " is a required field."
