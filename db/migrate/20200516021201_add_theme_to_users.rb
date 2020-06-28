63a8cef374d7663af9cced4913c201b18c0a9586
Tam Nguyen <43654181+tamnguyen98@users.noreply.github.com>
2020-06-27 23:56:09 -0700

    Update 20200516021201_add_theme_to_users.rb
    Added Not Null requirement
    Added From To as requested

--- db/migrate/20200516021201_add_theme_to_users.rb
+++ db/migrate/20200516021201_add_theme_to_users.rb
@@ -1,6 +1,7 @@
 class AddThemeToUsers < ActiveRecord::Migration[5.2]
   def change
     add_column :users, :theme, :string
-    change_column_default :users, :theme, "default"
+    change_column_null :users, :theme, false
+    change_column_default :users, :theme, from: "", to: "default"
   end
 end
