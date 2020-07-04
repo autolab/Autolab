728c428600088454ddec605dff1bf2c059c21152
Victor Huang <victorhuangwq@gmail.com>
2020-07-04 21:56:24 +0800

    Removes extra application.js, changed link_to to button_to

--- app/assets/javascripts/manage_submissions.js
+++ app/assets/javascripts/manage_submissions.js
@@ -96,8 +96,8 @@ $(document).ready(function() {
   });
 
   $('#submissions').on("click", ".submission-row", function(e) {
-    // Don't toggle row if we originally clicked on an anchor tag
-    if(e.target.localName != 'a') {
+    // Don't toggle row if we originally clicked on an anchor and input tag
+    if(e.target.localName != 'a' && e.target.localName !='input') {
       // e.target: tightest element that triggered the event
       // e.currentTarget: element the event has bubbled up to currently
       var submissionId = parseInt(e.currentTarget.id.replace("row-", ""), 10);
@@ -112,12 +112,4 @@ $(document).ready(function() {
     e.stopPropagation();
   });
 
-  $('.regrade-override').click(function(e) {
-    // Because regrade requests are sent with `data-method="post"`, we need to
-    // trick the link into behaving... like a link. When holding down Ctrl or
-    // Cmd, the regrade should open in a new tab.
-    if (e.metaKey || e.ctrlKey) {
-      $(this).attr('target', '_blank');
-    }
-  });
 });
