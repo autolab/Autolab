fa3241df365d51399c01cee0c1072c117b3367f1
Victor Huang <victorhuangwq@gmail.com>
2020-08-22 11:56:06 +0800

    Adds ability to export and import embedded forms (#1196)
    * Add ability to export and import embedded forms
    
    * remove redundant whitespace
    
    Co-authored-by: Fan Pu <fanpu@users.noreply.github.com>

--- app/controllers/assessments_controller.rb
+++ app/controllers/assessments_controller.rb
@@ -179,6 +179,7 @@ class AssessmentsController < ApplicationController
   def importAssessment
     @assessment = @course.assessments.new(name: params[:assessment_name])
     @assessment.load_yaml # this will save the assessment
+    @assessment.load_embedded_quiz # this will check and load embedded quiz
     @assessment.construct_folder # make sure there's a handin folder, just in case
     @assessment.load_config_file # only call this on saved assessments
     redirect_to([@course, @assessment])
@@ -348,6 +349,8 @@ class AssessmentsController < ApplicationController
     begin
       # Update the assessment config YAML file.
       @assessment.dump_yaml
+      # Save embedded_quiz
+      @assessment.dump_embedded_quiz
       # Pack assessment directory into a tarball.
       tarStream = StringIO.new("")
       Gem::Package::TarWriter.new(tarStream) do |tar|
