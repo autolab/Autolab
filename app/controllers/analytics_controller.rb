77ac46dffa232f002e838028e73debfa150769b6
amartinez116 <amartin1@andrew.cmu.edu>
2020-10-14 23:16:14 -0400

    groundwork for new analytics page

---
+++ app/controllers/analytics_controller.rb
@@ -0,0 +1,5 @@
+class AnalyticsController < ApplicationController
+	action_auth_level :index, :instructor
+	def index
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -69,6 +69,9 @@ Rails.application.routes.draw do
         get "run"
     end
 
+    resources :analytics, only: :index do
+    end
+
     resources :jobs, only: :index do
       get "getjob", on: :member
 
