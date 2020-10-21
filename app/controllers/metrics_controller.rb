a4de7b6c3d62d47cfa9181d9f2adb4ee5fb48a6c
amartinez116 <31053044+amartinez116@users.noreply.github.com>
2020-10-21 12:53:51 -0400

    Metrics page (#1225)
    * Annotation position bug fixes
    
    * Revert "Annotation position bug fixes"
    
    This reverts commit c5a48b38f17912c14d6b0ba562401da694d2c141.
    
    * Annotation position bug fixes
    
    * Gemfile updates - done automatically by bundle
    
    * groundwork for new analytics page
    
    * semantic ui now available in analytics page
    
    * Update analytics.scss
    
    * included semantic ui javaScript
    
    * updated name from analytics to metrics
    
    * Update Gemfile
    
    * Update metrics.scss
    
    Co-authored-by: Oliver Li Yicheng <liyicheng340@gmail.com>

---
+++ app/controllers/metrics_controller.rb
@@ -0,0 +1,5 @@
+class MetricsController < ApplicationController
+	action_auth_level :index, :instructor
+	def index
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -69,6 +69,9 @@ Rails.application.routes.draw do
         get "run"
     end
 
+    resources :metrics, only: :index do
+    end
+
     resources :jobs, only: :index do
       get "getjob", on: :member
 
