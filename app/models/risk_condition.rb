730c16491d64e7326c71ff611efdcfd53aaef95e
xinyis991105 <xinyis@andrew.cmu.edu>
2020-10-25 10:55:15 +0800

    changed wording of variables to avoid confusion in the future

--- app/models/risk_condition.rb
+++ app/models/risk_condition.rb
@@ -4,14 +4,14 @@ class RiskCondition < ApplicationRecord
   has_many :watchlist_instances, dependent: :destroy
   belongs_to :course
 
-  LATE_DAY_USAGE = 1 # :late_threshold, :date
+  GRACE_DAY_USAGE = 1 # :grace_day_threshold, :date
   GRADE_DROP = 2 # :percentage_drop, :consecutive_counts
   NO_SUBMISSIONS = 3 # :no_submissions_threshold
   LOW_GRADES = 4 # :grade_threshold, :count_threshold
 
   def self.create_condition_for_course_with_type(course_id, type, params)
   	# parameter check shouldn't surface to user and is for debug only
-    if (type == LATE_DAY_USAGE && (params[:late_threshold].nil? || params[:date].nil? || params.length != 2)) ||
+    if (type == GRACE_DAY_USAGE && (params[:grace_day_threshold].nil? || params[:date].nil? || params.length != 2)) ||
        (type == GRADE_DROP && (params[:percentage_drop].nil? || params[:consecutive_counts].nil? || params.length != 2)) ||
        (type == NO_SUBMISSIONS && (params[:no_submissions_threshold].nil? || params.length != 1)) ||
        (type == LOW_GRADES && (params[:grade_threshold].nil? || params[:count_threshold].nil? || params.length != 2))
