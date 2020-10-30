c96a122c5a0de2c23fc69cfd5be7cf3c84120175
xinyis991105 <54905587+xinyis991105@users.noreply.github.com>
2020-10-30 21:10:29 +0800

    API endpoint condition retrieval (#1234)
    * add endpoints for getting current metrics conditions
    
    * fixed table column name issue; updated methods to reflect that change
    
    * changed the returned type from active record to json; changed the output condition type to string; handle simple error
    
    * updated parameters obtaining and documentation comments
    
    * improved enum
    
    * Adds routing to get_current_metrics
    
    * modified the endpoint to use course name as an identifier
    
    Co-authored-by: Victor Huang <victorhuangwq@gmail.com>

--- app/controllers/metrics_controller.rb
+++ app/controllers/metrics_controller.rb
@@ -2,4 +2,23 @@ class MetricsController < ApplicationController
 	action_auth_level :index, :instructor
 	def index
 	end
+
+	action_auth_level :get_current_metrics, :instructor
+	def get_current_metrics
+		# This API endpoint aims to retrieve the current/latest risk conditions for a particular course
+		# On success, a JSON list of condition objects will be returned
+		# The type of each object is specified in a field called "condition_type"
+		# Possible types include: grace_day_usage, grade_drop, no_submission, low_grades
+		# Other fields for a risk condition object include parameters, version, created_at, updated_at, and course_id
+		# In particular, the parameters field includes specific information of the condition corresponding to its type
+		# On error, a flash error message will be shown and nil gets returned
+		begin
+			course_name = params[:course_name]
+			conditions = RiskCondition.get_current_for_course(course_name)
+			render json: conditions
+		rescue => error
+			flash[:error] = error.message
+			return
+		end
+	end
 end
