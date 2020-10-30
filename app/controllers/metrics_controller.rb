987b29e0bb38a2f2d478babcfb7d4e34ed432e94
xinyis991105 <xinyis@andrew.cmu.edu>
2020-10-30 14:34:33 +0800

    updated parameters obtaining and documentation comments

--- app/controllers/metrics_controller.rb
+++ app/controllers/metrics_controller.rb
@@ -3,19 +3,24 @@ class MetricsController < ApplicationController
 	def index
 	end
 
-	def get_current_metrics(course_id)
-		# GRACE_DAY_USAGE = 1 # :grace_day_threshold, :date
-		# GRADE_DROP = 2 # :percentage_drop, :consecutive_counts
-		# NO_SUBMISSIONS = 3 # :no_submissions_threshold
-		# LOW_GRADES = 4 # :grade_threshold, :count_threshold
+	def get_current_metrics
+		# This API endpoint aims to retrieve the current/latest risk conditions for a particular course
+		# User is expected to include in the parameters the course's ID with :id being the key
+		# On success, a JSON list of condition objects will be returned
+		# The type of each object is specified in a field called "condition_type"
+		# Possible types include: GRACE_DAY_USAGE, GRADE_DROP, NO_SUBMISSIONS, LOW_GRADES
+		# Other fields for the risk condition object include: parameters, version, created_at, updated_at, and course_id
+		# On error, a flash error message will be shown and nothing gets returned (i.e. nil gets returned)
 		begin
+			course_id = params[:id]
 			conditions = RiskCondition.get_current_for_course(course_id)
 			conditions_json_string = conditions.to_json
 			conditions_json = JSON.parse(conditions_json_string)
 			conditions_json.map! { |c| c["condition_type"] = type_string_of_enum(c["condition_type"]); c }
 			render json: conditions_json
 		rescue => error
-			return error.message
+			flash[:error] = error.message
+			return
 		end
 	end
 
@@ -30,7 +35,7 @@ private
 		elsif t == 4
 			return "LOW_GRADES"
 		else
-			raise "Invalid condition type!"
+			raise "Condition retrieval failed! Got invalid condition type for metric conditions!"
 		end
 	end
 end
