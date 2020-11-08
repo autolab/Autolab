652fd3fb8f3d17a664c065b1f62f7ddb4f196320
amartinez116 <amartin1@andrew.cmu.edu>
2020-11-07 22:50:42 -0500

    changed icon size and checkbox refresh logic

--- app/assets/javascripts/metrics.js
+++ app/assets/javascripts/metrics.js
@@ -2,13 +2,15 @@
 //= require semantic-ui
 $(document).ready(function(){
     $('.tabular.menu .item').tab();
-    $('.ui.dropdown').dropdown();
-    $('.ui.checkbox').checkbox();
+		$('.ui.dropdown').dropdown();
+		$('.ui.checkbox').checkbox();
+		$('.ui.calendar').calendar({type: 'date'});
     $('.ui.form')
 	  .form({
+			inline: true,
 	    fields: {
-	      percentage: {
-	        identifier: 'percentage',
+	      percentage1: {
+	        identifier: 'percentage1',
 	        optional: 'true',
 	        rules: [
 	          {
@@ -16,8 +18,39 @@ $(document).ready(function(){
 	            prompt : 'Please enter an integer from 0 to 100 for percentages'
 	          }
 	        ]
-      	  }
+				},
+				percentage2: {
+	        identifier: 'percentage2',
+	        optional: 'true',
+	        rules: [
+	          {
+	            type   : 'integer[0..100]',
+	            prompt : 'Please enter an integer from 0 to 100 for percentages'
+	          }
+	        ]
+				},
 	    }
-	  })
-	;
+		});
+
+		$('[name="metrics-checkbox-1"]').change(function() {
+			$('#save').removeClass('disabled');
+		});
+
+		$('[name="metrics-checkbox-2"]').change(function() {
+			$('#save').removeClass('disabled');
+		});
+
+		$('[name="metrics-checkbox-3"]').change(function() {
+			$('#save').removeClass('disabled');
+		});
+
+		$('[name="metrics-checkbox-4"]').change(function() {
+			$('#save').removeClass('disabled');
+		});
+		
+		$(window).bind('beforeunload', function(){
+			if (!$('#save').hasClass('disabled')) {
+				return 'Make sure to save your changes';
+			}
+		});
 });
