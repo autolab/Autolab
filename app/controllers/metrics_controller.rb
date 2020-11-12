44aa34f5591672ddd929b51c51e93ac7cc0ef8d6
xinyis991105 <54905587+xinyis991105@users.noreply.github.com>
2020-11-13 00:32:30 +0800

    API endpoint for updating metrics condition (#1241)
    * changes
    
    * api update risk conditions v1
    
    * deleted nonsense making api
    
    * tmp
    
    * enable testing with fetch API
    
    * render appropriately on error; raise error on malformed body; combine duplicate cases

--- app/controllers/metrics_controller.rb
+++ app/controllers/metrics_controller.rb
@@ -17,7 +17,7 @@ class MetricsController < ApplicationController
 			conditions = RiskCondition.get_current_for_course(course_name)
 			render json: conditions
 		rescue => error
-			flash[:error] = error.message
+			render :text => 'Not Found', :status => '404'
 			return
 		end
 	end
@@ -41,4 +41,45 @@ class MetricsController < ApplicationController
 		end
 	end
 
+	action_auth_level :update_current_metrics, :instructor
+	def update_current_metrics
+		# This API endpoint aims to update current/latest risk conditions for a particular course
+		# On success, a JSON list of condition objects that are freshly created out of the request parameters will be returned
+		# The request parameters must take on the following format:
+		# params[type_a] is nil if instructor does not want to include a risk condition of type_a during update
+		# params[type_a] is an object satisfying the parameter requirement for type_a condition
+		# e.g. params["grace_day_usage"] should have form { "grace_day_threshold" => 3, "date" => "2020-03-05" }
+		# Currently, four types of risk conditions are supported:
+		# "grace_day_usage" with parameters "grace_day_threshold", "date"
+		# "grade_drop" with parameters "percentage_drop", "consecutive_counts"
+		# "no_submissions" with parameter "no_submissions_threshold"
+		# "low_grades" with parameters "grade_threshold", "count_threshold"
+		# On error, a flash error message will be rendered and nil gets returned
+		begin
+			course_name = params[:course_name]
+			params_filtered = new_metrics_params
+			if params_filtered.nil?
+				params_filtered = {}
+			else
+				params_filtered = params_filtered.to_h
+			end
+			if params_filtered != params[:metric]
+				raise "Invalid update parameters for risk conditions! Make sure your request body fits the criteria!"
+			end
+			conditions = RiskCondition.update_current_for_course(course_name, params_filtered)
+			render json: conditions
+		rescue => error
+			render json: { error: error.message }, status: :bad_request
+			return
+		end
+	end
+
+private
+	
+	def new_metrics_params
+		return params.require(:metric).permit(:grace_day_usage => [:grace_day_threshold, :date],
+											  :grade_drop => [:percentage_drop, :consecutive_counts],
+											  :no_submissions => [:no_submissions_threshold],
+											  :low_grades => [:grade_threshold, :count_threshold]) if params[:metric].present?
+	end
 end
