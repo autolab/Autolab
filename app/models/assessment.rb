0558ea5217c1606743544bb00408c18988de5963
xinyis991105 <xinyis@andrew.cmu.edu>
2020-12-06 13:33:52 +0800

    removed update for loading config files

--- app/models/assessment.rb
+++ app/models/assessment.rb
@@ -227,8 +227,6 @@ class Assessment < ApplicationRecord
     # config file might have an updated custom raw score function: clear raw score cache
     invalidate_raw_scores
 
-    update_course_grade_watchlist_instances_if_past_end_at
-
     logger.info "Loaded #{config_file_path}"
   end
 
