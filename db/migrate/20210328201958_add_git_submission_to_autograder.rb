30680ba10da932f2b41163cddb20883c0d679dcc
Fan Pu <fanpu@users.noreply.github.com>
2021-04-05 05:48:54 +0800

    Set git_enabled to false by default in migration (#1313)

--- db/migrate/20210328201958_add_git_submission_to_autograder.rb
+++ db/migrate/20210328201958_add_git_submission_to_autograder.rb
@@ -1,6 +1,6 @@
 class AddGitSubmissionToAutograder < ActiveRecord::Migration[5.2]
   def change
-    add_column :autograders, :git_enabled, :boolean
+    add_column :autograders, :git_enabled, :boolean, :default => false
     add_column :autograders, :git_assignment_name, :string
   end
 end
