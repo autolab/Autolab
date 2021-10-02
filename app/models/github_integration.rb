39d5ed6eaf161e6a3fdc78b84b3e80ee490b6830
fanpu <zengfanpu@gmail.com>
2021-10-02 15:01:28 -0400

    Add default_branch info to repos returned, and also sort by recently updated

--- app/models/github_integration.rb
+++ app/models/github_integration.rb
@@ -1,12 +1,22 @@
 class GithubIntegration < ApplicationRecord
   belongs_to :user
 
+  # Returns the top 30 most recently pushed repos
+  # Reasonably if a user wants to submit code, it should be among
+  # the most recent repos updated...
   def repositories
     client = Octokit::Client.new(:access_token => access_token)
-    repos = client.repos()
+    repos = client.repos({},
+      query: {
+        sort:  "pushed",
+        per_page:  30
+      }
+    )
     repos.map { |repo|  
       {full_name: repo[:full_name], 
-        clone_url: repo[:clone_url]}
+        clone_url: repo[:clone_url],
+        default_branch: repo[:default_branch],
+      }
     }
   end
 end
