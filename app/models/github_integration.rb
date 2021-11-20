08acfadac99d33164a89c8d4ec90af2a15e3f72e
Fan Pu <fanpu@users.noreply.github.com>
2021-11-20 15:19:47 -0500

    Bug fixes and enhancements for github oauth2 (#1421)
    * enforce uniqueness of user ID on github integration to ensure only 1 is created per user, and also fail gracefully if user cancels oauth workflow
    
    * check if repo is too big before cloning
    
    * better helptext
    
    Co-authored-by: ashleyzhang <ashleyzhang@gmail.com>

--- app/models/github_integration.rb
+++ app/models/github_integration.rb
@@ -47,10 +47,14 @@ class GithubIntegration < ApplicationRecord
   # repo_name is of the form user/repo
   # clone_url is of the form https://github.com/user/repo.git
   #
-  def clone_repo(repo_name)
+  def clone_repo(repo_name, max_size)
     client = Octokit::Client.new(:access_token => access_token)
     repo_info = client.repo(repo_name)
     clone_url = repo_info[:clone_url]
+
+    if repo_info[:size] * 1000 > max_size
+      raise "Repository size too large, please ensure that you are not checking in unnecessary files"
+    end
 
     if self.access_token.nil? or self.access_token.empty?
       raise "Account not connected to Github"
