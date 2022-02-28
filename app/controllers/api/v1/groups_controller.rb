d21caa63b82ec6e1b786d05c92f66944fd255359
Victor Huang <victorhuangwq@gmail.com>
2022-02-27 23:38:37 -0500

    linted controller

--- app/controllers/api/v1/groups_controller.rb
+++ app/controllers/api/v1/groups_controller.rb
@@ -10,32 +10,32 @@ class Api::V1::GroupsController < Api::V1::BaseApiController
 
     respond_with({ group_size: group_size,
                    groups: groups,
-                   assessment: @assessment})
+                   assessment: @assessment })
   end
 
   # create group endpoint
   def create
     require_params([:groups])
 
-    if(@assessment.group_size <= 1)
+    if @assessment.group_size <= 1
       raise ApiError.new("Group size of assessment is currently 1,"\
                          " so you can't create group", :bad_request)
     end
     group_members = params[:groups].map{ |group| group["group_members"] }
 
-    if(!group_members&.select{|list| list.length > @assessment.group_size }.empty?)
+    if !group_members&.select{ |list| list.length > @assessment.group_size }&.empty?
       raise ApiError.new("Cannot have groups with size more than group_size", :bad_request)
     end
 
     all_emails = params[:groups].map{ |group| group["group_members"] }.flatten.uniq
     all_cuds = @course.course_user_data.joins(:user).where(users: { email: all_emails })
-    
+
     all_cud_ids = all_cuds.map(&:id)
     all_auds = @assessment.assessment_user_data.where(course_user_datum_id: all_cud_ids )
 
     email_to_cuds = all_cuds.map{ |cud| { cud.email => cud } }.reduce({}, :merge)
     cuds_to_auds =  all_auds.map{ |aud| { aud.course_user_datum_id => aud } }.reduce({}, :merge)
-    
+
     params[:groups].each do |g|
       group = Group.new
       group.name = g["name"] || "Unnamed Group"
@@ -43,6 +43,7 @@ class Api::V1::GroupsController < Api::V1::BaseApiController
       g["group_members"].each do |group_member|
         aud = cuds_to_auds[email_to_cuds[group_member]&.id]
         next unless aud&.group_confirmed(AssessmentUserDatum::UNCONFIRMED)
+
         aud.group = group
         aud.membership_status = AssessmentUserDatum::CONFIRMED
         count += 1 if aud.save!
@@ -50,16 +51,17 @@ class Api::V1::GroupsController < Api::V1::BaseApiController
       group.save! if count > 0
     end
 
-   respond_with_hash(@assessment.groups)
+    respond_with_hash(@assessment.groups)
   end
 
   # endpoint to delete groups
   def destroy
     require_params([:id])
     group = @assessment.groups.find_by(id: params[:id].to_i)
-    if(group.nil?)
+    if group.nil?
       raise ApiError.new("Couldn't find group #{params[:id]}", :bad_request)
     end
+
     ActiveRecord::Base.transaction do
       group.assessment_user_data.each do |aud|
         aud.group_id = nil
@@ -68,7 +70,6 @@ class Api::V1::GroupsController < Api::V1::BaseApiController
       end
       group.destroy!
     end
-    respond_with_hash(message:"Group #{params[:id]} successfully deleted")
+    respond_with_hash(message: "Group #{params[:id]} successfully deleted")
   end
-
 end
