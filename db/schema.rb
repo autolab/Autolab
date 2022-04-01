4d52c83b51fda0f17023643b858895e6f20a32bf
xinyis991105 <xinyis@andrew.cmu.edu>
2022-04-01 13:16:50 -0400

    remove extra schema lines

--- db/schema.rb
+++ db/schema.rb
@@ -373,8 +373,4 @@ ActiveRecord::Schema.define(version: 2022_03_30_160300) do
     t.index ["risk_condition_id"], name: "index_watchlist_instances_on_risk_condition_id"
   end
 
-  add_foreign_key "github_integrations", "users"
-  add_foreign_key "oauth_access_grants", "oauth_applications", column: "application_id"
-  add_foreign_key "oauth_access_tokens", "oauth_applications", column: "application_id"
-  add_foreign_key "oauth_device_flow_requests", "oauth_applications", column: "application_id"
 end
