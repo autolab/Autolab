32bea148f62948fa254c5b446d9c1008eaa47c06
Victor Huang <victorhuangwq@gmail.com>
2022-04-17 19:40:18 -0400

    Change to sort by updated_at in descending

--- app/controllers/annotations_controller.rb
+++ app/controllers/annotations_controller.rb
@@ -56,7 +56,9 @@ class AnnotationsController < ApplicationController
   action_auth_level :shared_comments, :course_assistant
   def shared_comments
     result = Annotation.select("annotations.id, annotations.comment")
-                       .joins(@submissions).where(shared_comment: true).as_json
+                       .joins(@submissions).where(shared_comment: true)
+                       .order(updated_at: :desc).limit(50).as_json
+
     render json: result, status: :ok
   end
 
