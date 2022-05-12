69348055c6ac1c10c766317bb4446a41bd791a9a
Victor Huang <victorhuangwq@gmail.com>
2022-05-12 12:25:33 -0400

    Update namechange of bulk_release (#1518)

--- config/routes.rb
+++ config/routes.rb
@@ -188,7 +188,7 @@ Rails.application.routes.draw do
 
     resources :course_user_data do
       resource :gradebook, only: :show do
-        get "bulkRelease"
+        get "bulk_release"
         get "csv"
         get "invalidate"
         get "statistics"
@@ -204,7 +204,7 @@ Rails.application.routes.draw do
     end
 
     member do
-      get "bulkRelease"
+      get "bulk_release"
       get "download_roster"
       match "email", via: [:get, :post]
       get "manage"
