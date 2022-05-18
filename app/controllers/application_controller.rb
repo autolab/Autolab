7ddc917679c0155c64897a1162c0433b20b00fe0
Damian Ho <damian_ho_xu_yang@yahoo.com>
2022-05-18 14:18:31 -0400

    raise: false

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -77,12 +77,12 @@ class ApplicationController < ActionController::Base
   def self.action_no_auth(action)
     skip_before_action :verify_authenticity_token, only: [action], raise: false
     skip_before_action :authenticate_user!, only: [action], raise: false
-    skip_before_action :configure_permitted_paramters, only: [action]
-    skip_before_action :maintenance_mode?, only: [action]
-    skip_before_action :run_scheduler, only: [action]
+    skip_before_action :configure_permitted_paramters, only: [action], raise: false
+    skip_before_action :maintenance_mode?, only: [action], raise: false
+    skip_before_action :run_scheduler, only: [action], raise: false
 
     skip_before_action :authenticate_user, only: [action], raise: false
-    skip_before_action :authorize_user_for_course, only: [action]
+    skip_before_action :authorize_user_for_course, only: [action], raise: false
     skip_before_action :authenticate_for_action, only: [action], raise: false
     skip_before_action :update_persistent_announcements, only: [action], raise: false
   end
