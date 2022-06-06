686f817e0ffab0bbf03bcfafb3a08c3d00a631a4
Damian Ho <damian_ho_xu_yang@yahoo.com>
2022-06-05 22:12:26 -0400

    Integrate changes from #1532

--- app/controllers/course_user_data_controller.rb
+++ app/controllers/course_user_data_controller.rb
@@ -190,14 +190,7 @@ class CourseUserDataController < ApplicationController
       redirect_to([@cud.course]) && return
     end
 
-    @users = {}
-    @usersEncoded = {}
-    @course.course_user_data.each do |cud|
-      # Prevent XSS inside autocomplete
-      @users[CGI.escapeHTML cud.full_name_with_email] = cud.id
-      # Why base64? See issue 931
-      @usersEncoded[Base64.urlsafe_encode64(cud.full_name_with_email.strip).strip] = cud.id
-    end
+    @users, @usersEncoded = @course.get_autocomplete_data
 
     return unless request.post?
 
