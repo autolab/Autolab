394e0a59f25f85e916985755aae777af6e06175b
Nicholas Clark <nicholas@californiaclarks.com>
2022-10-10 02:37:45 -0400

    Fixed annotation switching problems not updating score

--- app/controllers/annotations_controller.rb
+++ app/controllers/annotations_controller.rb
@@ -34,6 +34,10 @@ class AnnotationsController < ApplicationController
     tweaked_params.delete(:submission_id)
     tweaked_params.delete(:filename)
     ActiveRecord::Base.transaction do
+      # Remove effect of annotation to handle updating annotation problem
+      @annotation.update({ "value" => "0" })
+      @annotation.update_non_autograded_score
+
       @annotation.update(tweaked_params)
       @annotation.update_non_autograded_score
     end
