2e75a7c2a1fbd5e84d903e54ca8b0493955a3021
Michelle <mliuu3259@gmail.com>
2022-10-12 15:49:59 -0400

    smoother scroll

--- app/assets/javascripts/feedback.js
+++ app/assets/javascripts/feedback.js
@@ -1,8 +1,8 @@
 $(window).scroll(function(e){ 
   const $el = $('.result-fixed'); 
-  const containerWidth = $('.result-container')[0].offsetWidth;
-  console.log(containerWidth);
-  if ($(this).scrollTop() > 250){ 
+  const container = $('.result-container')[0];
+  const containerWidth = container.offsetWidth;
+  if (container.getBoundingClientRect().top <= 30){ 
     $el.css({'position': 'fixed', 'top': '30px', width: `${containerWidth * .33}px`}); 
   } else {
     $el.css({'position': 'absolute', 'top': '0px'}); 
