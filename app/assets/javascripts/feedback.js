83b199c8dc7f8d52c28e66bd22656fc2b8ca70d3
Michelle <mliuu3259@gmail.com>
2022-10-13 15:34:53 -0400

    added additional comments to code, changed dropshadow to border

--- app/assets/javascripts/feedback.js
+++ app/assets/javascripts/feedback.js
@@ -4,7 +4,10 @@ $(window).scroll(function(e){
   const container = $('.result-container')[0];
   const containerWidth = container.offsetWidth;
   if (container.getBoundingClientRect().top <= 30){ 
-    $el.css({'position': 'fixed', 'top': '30px', width: `${containerWidth * .33}px`}); 
+    // set to be 25% of feedback container width instead of entire page
+    // (which is what happens if you use % for a fixed div)
+    // if the .25 is changed, also change the width for .result-summary class
+    $el.css({'position': 'fixed', 'top': '30px', width: `${containerWidth * .25}px`}); 
   } else {
     $el.css({'position': 'absolute', 'top': '0px'}); 
   } 
