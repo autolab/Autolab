b5d39ae2809be7785fe0c5480bcacf802711c17a
Joey Wildman <josephwildman88@gmail.com>
2022-10-17 15:23:17 -0400

    Play around with Canvas API / NRPS

--- app/controllers/lti_integration_controller.rb
+++ app/controllers/lti_integration_controller.rb
@@ -7,33 +7,47 @@ class LtiIntegrationController < ApplicationController
 
   # have to do because we are making a POST request from Canvas
   skip_before_action :verify_authenticity_token
-  action_auth_level :launch, :student
+  action_auth_level :launch, :instructor
   def launch
+    # check that consumer key from canvas is correct
     if not Rails.configuration.lti_settings[params[:oauth_consumer_key]]
-      puts("HELLO")
       render json: { error: "we did not receive the oauth" }, status: :unauthorized
       return
     end
-    puts("got consumer key")
+
+    # required for oauth to work
     require 'oauth/request_proxy/action_controller_request'
-
-
+    # create "Tool Provider" using IMS LTI gem
     @provider = IMS::LTI::ToolProvider.new(
       params[:oauth_consumer_key],
       Rails.configuration.lti_settings[params[:oauth_consumer_key]],
       params
     )
+
     if not @provider.valid_request?(request)
       # the request wasn't validated
       render :launch_error, status: 401
       return
     end
-    puts("AUTH COMPLETE")
-    puts(@provider.username)
-    @return_url = "https:" + params[:custom_canvas_api_domain] + "/"
 
-    #response = Faraday.get("")
-    render json: { body: params }, status: :ok
-    #redirect_to(@return_url)
+    # just testing using the Canvas API, above flow not even necessary for this
+    # because using manually generated Canvas key
+    conn = Faraday.new(
+      "https://canvas.cmu.edu/api/v1/",
+      headers: {'Authorization' => "Bearer 7752~Bqkw7YLmlSBYGQDV5oBgrMFOevjV8hDNV3gnjBWFJbVsHYrjBSEqdJORKlyk7ma0"}
+    ) do |f|
+      f.response :json
+    end
+    @response = conn.get("courses/28853/users").body;
+
+    puts(@response.to_s)
+    render :inline => "<pre><%= JSON.pretty_generate(@response) %></pre>"
+
+    # might use code in future
+    # get_service_request(params[:oauth_consumer_key],
+    #                     Rails.configuration.lti_settings[params[:oauth_consumer_key]],
+    #                     "https://canvas.cmu.edu/api/lti/courses/28853/names_and_roles?access_token=7752~UIBEUM9K5hYc60tuc4OZR8cixYYHXlEWOM9GYo9zKFdAPQlcKQx8FFwBgT5WpzAB")
+    #@response = request(:get, "https://canvas.cmu.edu/api/v1/courses/28853/users", {'Authorization' => "Bearer 7752~Bqkw7YLmlSBYGQDV5oBgrMFOevjV8hDNV3gnjBWFJbVsHYrjBSEqdJORKlyk7ma0"})
   end
+
 end
