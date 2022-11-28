3f8e4e9d34545399c715b9970311ccf428b612d8
Nicholas Clark <nicholas@californiaclarks.com>
2022-11-28 00:22:49 -0500

    Fixed schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_24_042718) do
+ActiveRecord::Schema.define(version: 2022_11_28_044321) do
 
   create_table "annotations", force: :cascade do |t|
     t.integer "submission_id"
