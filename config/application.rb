05dc5893066450abef9382992c748d33c59f29b5
Evan Shi <14984764+evanyeyeye@users.noreply.github.com>
2023-02-05 15:36:36 -0500

    Add checks for outdated .env files

--- config/application.rb
+++ config/application.rb
@@ -116,15 +116,16 @@ module Autolab3
 
     config.lti_settings = Rails.application.config_for(:lti_settings)
 
-    config.action_controller.default_url_options = { }
-    config.action_mailer.default_url_options = {}
-
     # Set application host for mailer
-    config.action_mailer.default_url_options = { host: ENV['MAILER_HOST'] }
+    config.action_mailer.default_url_options = { host: ENV['MAILER_HOST'] || "YOUR_APP_HOST" }
 
     # Configure the host and port of generated urls
+    config.action_controller.default_url_options = {}
+
+    ENV['DEFAULT_URL_HOST'] = "" if ENV['DEFAULT_URL_HOST'].nil?
+    ENV['DEFAULT_URL_PORT'] = "" if ENV['DEFAULT_URL_PORT'].nil?
     if !ENV['DEFAULT_URL_HOST'].empty? 
-      config.action_controller.default_url_options = { host: ENV['DEFAULT_URL_HOST'] }
+      config.action_controller.default_url_options[:host] = ENV['DEFAULT_URL_HOST']
       if ENV['DEFAULT_URL_PORT'].casecmp?("NONE") then
         config.action_controller.default_url_options[:port] = nil
       elsif !ENV['DEFAULT_URL_PORT'].empty?
