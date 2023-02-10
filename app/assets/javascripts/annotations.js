1063bd13fd894659b84f9c3debed1f2149759a8c
Lynn <lchaeryn5863@gmail.com>
2023-02-09 21:41:20 -0500

    Fix scrolling annotation bug

--- app/assets/javascripts/annotations.js
+++ app/assets/javascripts/annotations.js
@@ -342,7 +342,11 @@ function attachEvents() {
 function attachChangeFileEvents() {
   // Set up file switching to use the local cache
   function changeFileClickHandler(e) {
-    const wasCachedLocally = changeFile($(this).data("header_position"));
+    const headerURL = new URLSearchParams(location.search);
+    const currentHeader = parseInt(headerURL.get("header_position"));
+
+    const targetHeader = $(this).data("header_position");
+    const wasCachedLocally = (targetHeader === currentHeader) || changeFile(targetHeader);
     if (wasCachedLocally) {
       e.preventDefault();
       if ($(this).data("line")) {
