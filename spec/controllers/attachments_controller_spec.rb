b75bef3b16ef2286f7b74dca4173d344217d3fb6
Damian Ho <damian_ho_xu_yang@yahoo.com>
2023-02-19 14:42:04 -0500

    Use shared_examples for index

--- spec/controllers/attachments_controller_spec.rb
+++ spec/controllers/attachments_controller_spec.rb
@@ -5,56 +5,47 @@ RSpec.describe AttachmentsController, type: :controller do
   render_views
 
   # Course attachments
+
+  # INDEX
+  shared_examples "index_success" do |u, login: true|
+    login_as(u) if login
+    let!(:cid)  { get_course_id_by_uid(u.id) }
+    let!(:cname) { Course.find(cid).name }
+    it "renders successfully" do
+      get :index, params: {course_name: cname}
+      expect(response).to be_successful
+      expect(response.body).to match(cname)
+      expect(response.body).to match(/Course Attachments/m)
+    end
+  end
+
+  shared_examples "index_failure" do |u, login: true|
+    login_as(u) if login
+    let!(:cid)  { get_course_id_by_uid(u.id) }
+    let!(:cname) { Course.find(cid).name }
+    it "renders with failure" do
+      get :index, params: {course_name: cname}
+      expect(response).not_to be_successful
+      expect(response.body).not_to match(cname)
+      expect(response.body).not_to match(/Course Attachments/m)
+    end
+  end
+
   describe "#index" do
     context "when user is Autolab admin" do
-      u = get_admin
-      login_as(u)
-      cid = get_course_id_by_uid(u.id)
-      cname = Course.find(cid).name
-      it "renders successfully" do
-        get :index, params: {course_name: cname}
-        expect(response).to be_successful
-        expect(response.body).to match(cname)
-        expect(response.body).to match(/Course Attachments/m)
-      end
+      it_behaves_like "index_success", get_admin
     end
 
     context "when user is Autolab instructor" do
-      u = get_instructor
-      login_as(u)
-      cid = get_course_id_by_uid(u.id)
-      cname = Course.find(cid).name
-      it "renders successfully" do
-        get :index, params: {course_name: cname}
-        expect(response).to be_successful
-        expect(response.body).to match(cname)
-        expect(response.body).to match(/Course Attachments/m)
-      end
+      it_behaves_like "index_success", get_instructor
     end
 
     context "when user is Autolab user" do
-      u = get_user
-      login_as(u)
-      cid = get_course_id_by_uid(u.id)
-      cname = Course.find(cid).name
-      it "renders with failure" do
-        get :index, params: {course_name: cname}
-        expect(response).not_to be_successful
-        expect(response.body).not_to match(cname)
-        expect(response.body).not_to match(/Course Attachments/m)
-      end
+      it_behaves_like "index_failure", get_user
     end
 
     context "when user is not logged in" do
-      u = get_admin
-      cid = get_course_id_by_uid(u.id)
-      cname = Course.find(cid).name
-      it "renders with failure" do
-        get :index, params: {course_name: cname}
-        expect(response).not_to be_successful
-        expect(response.body).not_to match(cname)
-        expect(response.body).not_to match(/Course Attachments/m)
-      end
+      it_behaves_like "index_failure", get_admin, login: false
     end
   end
 
