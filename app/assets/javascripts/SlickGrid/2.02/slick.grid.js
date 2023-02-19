116762e5cd8468a4a87df9766a28e72da8db8d17
lykimchee <lchaeryn5863@gmail.com>
2023-02-19 14:34:39 -0500

    Gradebook sorting icon & styling (#1784)
    Updated sorting icon and gradebook styling

--- app/assets/javascripts/SlickGrid/2.02/slick.grid.js
+++ app/assets/javascripts/SlickGrid/2.02/slick.grid.js
@@ -565,7 +565,7 @@ if (typeof Slick === "undefined") {
 
         if (m.sortable) {
           header.addClass("slick-header-sortable");
-          header.append("<span class='slick-sort-indicator' />");
+          header.prepend("<span class='slick-sort-indicator' />");
         }
 
         trigger(self.onHeaderCellRendered, {
