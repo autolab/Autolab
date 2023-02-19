e2793a5eca6800f577c618a5c2ffd29f404cb3dc
Damian Ho <damian_ho_xu_yang@yahoo.com>
2023-02-19 14:47:23 -0500

    Use shared_examples for new

--- spec/controllers/attachments_controller_spec.rb
+++ spec/controllers/attachments_controller_spec.rb
@@ -49,60 +49,48 @@ RSpec.describe AttachmentsController, type: :controller do
     end
   end
 
+  # NEW
+  shared_examples "new_success" do |u, login: true|
+    login_as(u) if login
+    let!(:cid)  { get_course_id_by_uid(u.id) }
+    let!(:cname) { Course.find(cid).name }
+    it "renders successfully" do
+      get :new, params: {course_name: cname}
+      expect(response).to be_successful
+      expect(response.body).to match(cname)
+      expect(response.body).to match(/Name/m)
+      expect(response.body).to match(/Released/m)
+    end
+  end
+
+  shared_examples "new_failure" do |u, login: true|
+    login_as(u) if login
+    let!(:cid)  { get_course_id_by_uid(u.id) }
+    let!(:cname) { Course.find(cid).name }
+    it "renders with failure" do
+      get :new, params: {course_name: cname}
+      expect(response).not_to be_successful
+      expect(response.body).not_to match(cname)
+      expect(response.body).not_to match(/Name/m)
+      expect(response.body).not_to match(/Released/m)
+    end
+  end
+
   describe "#new" do
     context "when user is Autolab admin" do
-      u = get_admin
-      login_as(u)
-      cid = get_course_id_by_uid(u.id)
-      cname = Course.find(cid).name
-      it "renders successfully" do
-        get :new, params: {course_name: cname}
-        expect(response).to be_successful
-        expect(response.body).to match(cname)
-        expect(response.body).to match(/Name/m)
-        expect(response.body).to match(/Released/m)
-      end
+      it_behaves_like "new_success", get_admin
     end
 
     context "when user is Autolab instructor" do
-      u = get_instructor
-      login_as(u)
-      cid = get_course_id_by_uid(u.id)
-      cname = Course.find(cid).name
-      it "renders successfully" do
-        get :new, params: {course_name: cname}
-        expect(response).to be_successful
-        expect(response.body).to match(cname)
-        expect(response.body).to match(/Name/m)
-        expect(response.body).to match(/Released/m)
-      end
+      it_behaves_like "new_success", get_instructor
     end
 
     context "when user is Autolab user" do
-      u = get_user
-      login_as(u)
-      cid = get_course_id_by_uid(u.id)
-      cname = Course.find(cid).name
-      it "renders with failure" do
-        get :new, params: {course_name: cname}
-        expect(response).not_to be_successful
-        expect(response.body).not_to match(cname)
-        expect(response.body).not_to match(/Name/m)
-        expect(response.body).not_to match(/Released/m)
-      end
+      it_behaves_like "new_failure", get_user
     end
 
     context "when user is not logged in" do
-      u = get_admin
-      cid = get_course_id_by_uid(u.id)
-      cname = Course.find(cid).name
-      it "renders with failure" do
-        get :new, params: {course_name: cname}
-        expect(response).not_to be_successful
-        expect(response.body).not_to match(cname)
-        expect(response.body).not_to match(/Name/m)
-        expect(response.body).not_to match(/Released/m)
-      end
+      it_behaves_like "new_failure", get_admin, login: false
     end
   end
 
