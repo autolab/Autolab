faf4fa13a7052ea1c03cc3bce9d6d8ebf50c3307
Joanna Ge <joannage87@gmail.com>
2023-03-26 22:31:19 -0400

    Fix select all styling when checked

--- app/assets/javascripts/export_course.js
+++ app/assets/javascripts/export_course.js
@@ -1,22 +1,29 @@
+function addChecked(checkbox) {
+  const row = checkbox.closest('.course-field');
+
+  if (checkbox.checked) {
+    row.classList.add('checked');
+  } else {
+    row.classList.remove('checked');
+  }
+}
+
 $(document).ready(function() {
   const checkboxes = document.querySelectorAll('.cbox');
 
   checkboxes.forEach(function(checkbox) {
     checkbox.addEventListener('change', function() {
-      const row = checkbox.closest('.course-field');
-
-      if (checkbox.checked) {
-        row.classList.add('checked');
-      } else {
-        row.classList.remove('checked');
-      }
+      addChecked(checkbox);
     });
   });
 
+
   $('#select_all_btn').on('click', function() {
-    var fields = document.getElementsByClassName('cbox');
-    for(var i=0; i < fields.length; i++){  
-      fields[i].checked=true;  
+    var checkboxes = document.querySelectorAll('.cbox');
+    for(var i=0; i < checkboxes.length; i++){  
+      console.log(checkboxes[i]);
+      checkboxes[i].checked=true;
+      addChecked(checkboxes[i]);
     }  
   });
 });
