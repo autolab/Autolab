289caf5881f86c36aa950c179da8568a869c3211
Michelle <mliuu3259@gmail.com>
2023-03-27 13:12:37 -0400

    moved behavior to shared example

--- spec/controllers/annotations_controller_spec.rb
+++ spec/controllers/annotations_controller_spec.rb
@@ -4,38 +4,47 @@ include ControllerMacros
 RSpec.describe AnnotationsController, type: :controller do
   render_views
 
+  shared_examples "create_success" do
+    it "renders successfully" do
+      sign_in(user)
+
+      problem = assessment.problems.first
+      submission = assessment.submissions.first
+
+      annotation_params = {
+        filename: submission.filename, position: 0, line: 1,
+        submitted_by: user.id, shared_comment: false,
+        global_comment: false, problem_id: problem.id, coordinate: nil,
+        submission_id: submission.id, comment: "test", value: 0
+      }
+
+      post :create, params: { course_name: course.name,
+                              assessment_name: assessment.name,
+                              submission_id: submission.id,
+                              annotation: annotation_params }
+      expect(response).to be_successful
+    end
+  end
+
   describe "#create" do
     include_context "controllers shared context"
-    context "when user is instructor" do
-      it "renders successfully" do
-        sign_in(instructor_user)
 
-        problem = assessment.problems.first
-        submission = assessment.submissions.first
-
-        annotation_params = {
-          filename: submission.filename, position: 0, line: 1,
-          submitted_by: instructor_user.id, shared_comment: false,
-          global_comment: false, problem_id: problem.id, coordinate: nil,
-          submission_id: submission.id, comment: "test", value: 0
-        }
-
-        post :create, params: { course_name: course.name,
-                                assessment_name: assessment.name,
-                                submission_id: submission.id,
-                                annotation: annotation_params }
-        expect(response).to be_successful
+    context "when user is Autolab admin" do
+      it_behaves_like "create_success" do
+        let!(:user) { admin_user }
       end
     end
 
-    #  context "when user is not logged in" do
-    #    it "renders with failure" do
-    #      cid = get_first_cid_by_uid(student_user.id)
-    #      cname = Course.find(cid).name
-    #      get :report_bug, params: { name: cname }
-    #      expect(response).not_to be_successful
-    #      expect(response.body).not_to match(/Stuck on a bug/m)
-    #    end
-    #  end
+    context "when user is Autolab instructor" do
+      it_behaves_like "create_success" do
+        let!(:user) { instructor_user }
+      end
+    end
+
+    context "when user is Autolab course assistant" do
+      it_behaves_like "create_success" do
+        let!(:user) { course_assistant_user }
+      end
+    end
   end
 end
