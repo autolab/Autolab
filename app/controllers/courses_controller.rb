4b896538654c51c26af6d2aa74bdbdfc6792492b
Umar Alkafaween <omar.ka923@gmail.com>
2023-03-31 12:11:19 +0300

    Make moss action definition in one line

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -492,8 +492,7 @@ class CoursesController < ApplicationController
   end
 
   action_auth_level :moss, :instructor
-  def moss
-  end
+  def moss; end
 
   LANGUAGE_WHITELIST = %w[c cc java ml pascal ada lisp scheme haskell fortran ascii vhdl perl
                           matlab python mips prolog spice vb csharp modula2 a8086 javascript plsql
