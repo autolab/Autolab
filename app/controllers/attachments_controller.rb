bcc5039e51a376fdfc97777b99ad646ee75141c1
Damian Ho <damian_ho_xu_yang@yahoo.com>
2023-04-16 15:28:19 -0400

    Increase limit to 1GB

--- app/controllers/attachments_controller.rb
+++ app/controllers/attachments_controller.rb
@@ -25,8 +25,8 @@ class AttachmentsController < ApplicationController
 
   action_auth_level :create, :instructor
   def create
-    if params[:attachment][:file].size > 100.megabytes
-      flash[:error] = "Attachment file size must be less than 100 MB"
+    if params[:attachment][:file].size > 1.gigabyte
+      flash[:error] = "Attachment file size must be less than 1 GB"
       redirect_to_create_attachment && return
     end
 
