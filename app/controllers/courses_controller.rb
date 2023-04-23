18576de2dd7bf4fb2473f11faac5e6bbe9a8a34f
Damian Ho <damian_ho_xu_yang@yahoo.com>
2023-04-23 17:37:17 -0400

    Add user interface to upload config files, revamp file input fields (#1883)
    * Update handin helptext
    
    * Allow upload of assessment rb file
    
    * Restyle edit course page, remove (unused) name field
    
    * Allow upload of course rb file
    
    * Refine help-text
    
    * Automatically reload assessment config
    
    * Automatically reload course config
    
    * Update file_field helper for formbuilder
    
    * Minor style edits
    
    * Remove obsolete embedded form styling
    
    * Remove line from file selector
    
    * Add label_text option
    
    * Standardize help_text for course.rb

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -170,10 +170,25 @@ class CoursesController < ApplicationController
 
   action_auth_level :update, :instructor
   def update
-    flash[:error] = "Cannot update nil course" if @course.nil?
+    uploaded_config_file = params[:editCourse][:config_file]
+    unless uploaded_config_file.nil?
+      config_source = uploaded_config_file.read
+
+      course_config_source_path = @course.source_config_file_path
+      File.open(course_config_source_path, "w") do |f|
+        f.write(config_source)
+      end
+
+      begin
+        @course.reload_course_config
+      rescue StandardError, SyntaxError => e
+        @error = e
+        render("reload") && return
+      end
+    end
 
     if @course.update(edit_course_params)
-      flash[:success] = "Success: Course info updated."
+      flash[:success] = "Course configuration updated!"
     else
       flash[:error] = "Error: There were errors editing the course."
       @course.errors.full_messages.each do |msg|
