4e964ba50c4c85b9b60c35fa11051d4255117f7b
Victor Huang <victorhuangwq@gmail.com>
2023-04-24 11:56:36 -0400

    address general styling for score details

--- app/assets/javascripts/manage_submissions.js
+++ app/assets/javascripts/manage_submissions.js
@@ -2,121 +2,156 @@ const manage_submissions_endpoints = {
   'score_details': 'submissions/score_details',
 }
 
-$(document).ready(function() {
+$(document).ready(function () {
 
   $('.modal').modal();
 
-  $('.score-details').on('click', function() {
+  $('.score-details').on('click', function () {
     // Get the email 
     const course_user_datum_id = $(this).data('cuid');
     const email = $(this).data('email');
-    
+
+    // Set the email
+    $('#score-details-email').html(email);
+
     // Clear the modal content
     $('#score-details-content').html('');
-    $('#score-details-email').html(email);
-    
-    // TODO: Add loading spinner
+
+    // Add a loading bar
+    $('#score-details-content').html(`
+        <div class="progress">
+            <div class="indeterminate"></div>
+        </div>`);
 
     // Open the modal
     $('#score-details-modal').modal('open');
-    
+
     // Fetch data and render it in the modal 
-    getTableData(course_user_datum_id).then((data) => {
+    get_score_details(course_user_datum_id).then((data) => {
       console.log(data);
-      
-      const problemHeaders = data.submissions[0].problems.map((problem) => {
-        return `<th class="submission-th">${problem.name}</th>`;
+
+      const problem_headers = data.submissions[0].problems.map((problem) => {
+        const max_score = problem.max_score;
+        const autograded = problem.grader_id < 0 ? " (Autograded)" : "";
+        return `<th class="submission-th">
+                  ${problem.name}
+                  <br>
+                  ${max_score} ${autograded}
+                </th>`;
       }).join('');
 
-      const submissions = data.submissions.map((submission) => {
-          return `
+      const submissions_body = data.submissions.map((submission) => {
+        
+        let tweak_value = data?.tweaks[submission.id]?.value ?? "None";
+        if (tweak_value != "None" && tweak_value > 0) {
+          tweak_value = `+${tweak_value}`;
+        }
+
+        // Convert to human readable date with timezone 
+        const human_readable_created_at = moment(submission.created_at).format('MMM Do YYYY, h:mm:ss a z Z');
+
+        const view_button = submission.filename ? 
+              `<div class="submissions-center-icons">
+                  <a href="submissions/${submission.id}/view"
+                    title="View the file for this submission"
+                    class="btn small">
+                    <i class='material-icons'>zoom_in</i>
+                  </a>
+                  <p>View Source</p>
+                </div>` 
+                : "None";
+
+        const download_button = 
+                /text/.test(submission.detected_mime_type) ?
+                `<div class="submissions-center-icons">
+                    <a href="submissions/${submission.id}/download?forceMime=text/plain"
+                      title="Download as text/plain"
+                      class="btn small">
+                      <i class='material-icons'>file_download</i>
+                    </a>
+                    <p>Download</p>
+                  </div>` :
+                `<div class="submissions-center-icons">
+                    <a href="submissions/${submission.id}/download"
+                      title="Download the file for this submission"
+                      class="btn small">
+                      <i class='material-icons'>file_download</i>
+                    </a>
+                    <p>Download</p>
+                  </div>`;
+
+        return `
             <tr id="row-${submission.id}" class="submission-row">
               <td class="submission-td">
                 ${submission.version}
               </td>
-              <td class="submisison-td">
-                ${submission.created_at}
+              <td class="submission-td">
+                ${human_readable_created_at}
               </td>
               <td class="submission-td">
                 ${submission.total}
               </td>
-              ${submission.problems.map((problem) => {
-                return `<td class="submission-td">${data.scores[submission.id][problem.id]['score']}</td>`;
-              }).join('')}
+              ${submission.problems.
+              map((problem) =>
+                `<td class="submission-td">${data.scores[submission.id][problem.id]['score']}</td>`
+              ).join('')}
               <td class="submission-td">
                 ${submission.late_penalty}
               </td>
               <td class="submission-td">
                 <a href="submissions/${submission.id}/edit">
-                  ${data?.tweaks[submission.id]?.value ?? "None"}
+                  ${tweak_value}
                 </a>
               </td>
               <td class="submission-td">
-                ${submission.filename ?
-                  `<div class="submissions-center-icons">
-                    <a href="submissions/${submission.id}/view"
-                      title="View the file for this submission"
-                      class="btn small">
-                      <i class='material-icons'>zoom_in</i>
-                    </a>
-                    <p>View File</p>
-                  </div>` : "None"}
-                ${ /text/.test(submission.detected_mime_type) ?
-                    `<div class="submissions-center-icons">
-                      <a href="submissions/${submission.id}/download?forceMime=text/plain"
-                        title="Download as text/plain"
-                        class="btn small">
-                        <i class='material-icons'>file_download</i>
-                      </a>
-                      <p>Download as text/plain</p>
-                    </div>` :
-                    `<div class="submissions-center-icons">
-                      <a href="submissions/${submission.id}/download"
-                        title="Download the file for this submission"
-                        class="btn small">
-                        <i class='material-icons'>file_download</i>
-                      </a>
-                      <p>Download File</p>
-                    </div>`}
+                ${view_button}
+                ${download_button}
               </td>
             </tr>`;
       }).join('');
 
-      const submissionsTable = 
-        `<table class="prettyBorder" id="score-details-table">
+      const submissions_table =
+        ` <p>Click on non-autograded problem scores to edit or leave a comment </p>
+          <table class="prettyBorder" id="score-details-table">
             <thead>
               <tr>
                 <th class="submission-th">Version No.</th>
                 <th class="submission-th">Submission Date</th>
                 <th class="submission-th">Final Score</th>
-                  ${problemHeaders}
+                  ${problem_headers}
                 <th class="submission-th">Late Penalty</th>
                 <th class="submission-th">Tweak</th>
                 <th class="submission-th">Actions</th>
               </tr>
             </thead>
             <tbody>
-              ${submissions}
+              ${submissions_body}
             </tbody>
           </table>
         `;
 
-      $('#score-details-content').html(`<div> ${submissionsTable} </div>`);
+      $('#score-details-content').html(`<div> ${submissions_table} </div>`);
+      $('#score-details-table').DataTable({
+        "order": [[0, "desc"]],
+        "paging": false,
+        "info": false,
+        "searching": false,
+      });
     }).catch((err) => {
       console.log(err);
     });
   });
 
-  function getTableData(course_user_datum_id) {
+  function get_score_details(course_user_datum_id) {
     return new Promise((resolve, reject) => {
       $.ajax({
         url: manage_submissions_endpoints['score_details'],
         type: 'GET',
         data: { cuid: course_user_datum_id },
-        success: function(data) {
+        success: function (data) {
           resolve(data);
         },
-        error: function(err) {
+        error: function (err) {
           console.log(err);
           reject(err);
         }
@@ -189,7 +224,7 @@ $(document).ready(function() {
     return;
   });
 
-  $('#submissions').on("click", ".cbox", function(e) {
+  $('#submissions').on("click", ".cbox", function (e) {
     var submissionId = parseInt(e.currentTarget.id.replace("cbox-", ""), 10);
     toggleRow(submissionId);
     e.stopPropagation();
