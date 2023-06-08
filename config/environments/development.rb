2f716e11183ddfa0ada5f0f1ea78e73f5dbb5220
Damian Ho <damian_ho_xu_yang@yahoo.com>
2023-06-08 23:11:06 +0800

    Revert config.consider_all_requests_local

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -10,7 +10,7 @@ Rails.application.configure do
   config.eager_load = false
 
   # Show full error reports.
-  config.consider_all_requests_local = false
+  config.consider_all_requests_local = true
 
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
