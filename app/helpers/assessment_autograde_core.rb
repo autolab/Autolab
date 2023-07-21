557746a2d6fc5cc1695a941255ad51c1d17a45ae
Chaskiel Grundman <cg2v@andrew.cmu.edu>
2023-07-21 13:18:08 -0400

    Send Tango unambiguous filenames for user handins (#1947)
    * Export long handin filename
    
    * Allow autogradeInputFiles to provide remote name
    
    Allow autogradeInputFiles to provide a remoteFile attribute that will
    be used for the filename on the tango server. If remoteFile is not
    provided, the value of localFile will be used.
    
    * Use full filename for tango handin files.
    
    When uploading handins to tango, send the long filename (with the
    student's email address) as the filename to use on the tango server
    
    * sendJob remoteFile is always a basename
    
    Unlike localFile, remoteFile can always be a basename, so remove
    all the excess calls to File.basename

--- app/helpers/assessment_autograde_core.rb
+++ app/helpers/assessment_autograde_core.rb
@@ -44,17 +44,20 @@ module AssessmentAutogradeCore
         COURSE_LOGGER.log("Error while uploading autograding files for #{submission.id}: missing file #{name_of_file}")
         raise AutogradeError.new("Error while uploading autograding files", :missing_autograder_file, name_of_file)
       end
+      if f["remoteFile"].nil?
+        f["remoteFile"] = File.basename(f["localFile"])
+      end
     end
 
     # now actually send all of the upload requests
     upload_file_list.each do |f|
       md5hash = Digest::MD5.file(f["localFile"]).to_s
-      next if (existing_files.has_key?(File.basename(f["localFile"])) &&
-          existing_files[File.basename(f["localFile"])] == md5hash)
+      next if (existing_files.has_key?(f["remoteFile"]) &&
+          existing_files[f["remoteFile"]] == md5hash)
 
       begin
         TangoClient.upload("#{course.name}-#{assessment.name}",
-                           File.basename(f["localFile"]),
+                           f["remoteFile"],
                            File.open(f["localFile"], "rb").read)
       rescue TangoClient::TangoException => e
         COURSE_LOGGER.log("Error while uploading autograding files for #{submission.id}: #{e.message}")
@@ -156,7 +159,7 @@ module AssessmentAutogradeCore
   def tango_add_job(course, assessment, upload_file_list, callback_url, job_name, output_file)
     job_properties = { "image" => @autograde_prop.autograde_image,
                        "files" => upload_file_list.map do |f|
-                         { "localFile" => File.basename(f["localFile"]),
+                         { "localFile" => f["remoteFile"],
                            "destFile" => Pathname.new(f["destFile"]).basename.to_s }
                        end,
                        "output_file" => output_file,
@@ -328,6 +331,7 @@ module AssessmentAutogradeCore
     # of files needed by the autograder. Can be overridden in the
     # lab config file.
     local_handin = submission.handin_file_path
+    remote_handin = submission.handin_file_long_filename
     local_makefile = File.join(ass_dir, "autograde-Makefile")
     local_autograde = File.join(ass_dir, "autograde.tar")
 
@@ -335,7 +339,9 @@ module AssessmentAutogradeCore
     dest_handin = assessment.handin_filename
 
     # Construct the array of input files.
-    handin = { "localFile" => local_handin, "destFile" => dest_handin }
+    handin = { "localFile" => local_handin,
+               "remoteFile" => remote_handin,
+               "destFile" => dest_handin }
     makefile = { "localFile" => local_makefile, "destFile" => "Makefile" }
     autograde = { "localFile" => local_autograde, "destFile" => "autograde.tar" }
 
