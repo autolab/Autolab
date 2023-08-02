2334fd1db933cd97f4d23eb62f58aeb3e1573c5d
Damian Ho <damian_ho_xu_yang@yahoo.com>
2023-08-02 13:00:11 +0800

    Temporary fix for chromedriver issue

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -45,6 +45,10 @@ RSpec.configure do |config|
     Capybara::Selenium::Driver.new(app, browser: :chrome)
   end
 
+  # Temporary fix for chromedriver version issue, until we can update Selenium
+  # https://github.com/titusfortner/webdrivers/issues/247
+  Webdrivers::Chromedriver.required_version = "114.0.5735.90"
+
   Capybara.register_driver :headless_chrome do |app|
     capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
       'goog:chromeOptions': { args: %w[headless] }
