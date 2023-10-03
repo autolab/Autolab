25990d0aa9a1271e376fb1139466a0b90ade8679
Damian Ho <damian_ho_xu_yang@yahoo.com>
2023-10-03 16:13:47 -0400

    Fix calculation of effective_late_penalty and effective_version_penalty

--- app/models/assessment.rb
+++ app/models/assessment.rb
@@ -141,12 +141,20 @@ class Assessment < ApplicationRecord
   #
   # Use default course late penalty, if not set
   def effective_late_penalty
-    late_penalty || course.late_penalty
+    if !late_penalty.value.nil?
+      late_penalty
+    else
+      course.late_penalty
+    end
   end
 
   # Penalty to apply per version past the version_threshold
   def effective_version_penalty
-    version_penalty || course.version_penalty
+    if !version_penalty.value.nil?
+      version_penalty
+    else
+      course.version_penalty
+    end
   end
 
   # Submission version number past which to start applying version penalty
