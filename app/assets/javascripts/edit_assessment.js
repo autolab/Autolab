f5f212f3a3310a1cb570effedd13af71260070f8
Damian Ho <damian_ho_xu_yang@yahoo.com>
2023-10-25 19:15:24 -0400

    Clear / default values when checkbox clicked

--- app/assets/javascripts/edit_assessment.js
+++ app/assets/javascripts/edit_assessment.js
@@ -45,27 +45,50 @@
 
     // Penalties tab
     $('#unlimited_submissions').on('change', function() {
-      $('#assessment_max_submissions').prop('disabled', $(this).prop('checked'));
+      const checked = $(this).prop('checked');
+      const $max_submissions = $('#assessment_max_submissions');
+      $max_submissions.prop('disabled', checked);
+      if (checked) {
+        $max_submissions.val(-1);
+      }
     });
 
     $('#unlimited_grace_days').on('change', function() {
-      $('#assessment_max_grace_days').prop('disabled', $(this).prop('checked'));
+      const checked = $(this).prop('checked');
+      const $max_grace_days = $('#assessment_max_grace_days');
+      $max_grace_days.prop('disabled', checked);
+      if (checked) {
+        $max_grace_days.val('');
+      }
     });
 
     $('#use_default_late_penalty').on('change', function() {
+      const checked = $(this).prop('checked');
       const $latePenaltyField = $('#assessment_late_penalty_attributes_value').parent();
-      $latePenaltyField.find('input').prop('disabled', $(this).prop('checked'));
-      $latePenaltyField.find('select').prop('disabled', $(this).prop('checked'));
+      $latePenaltyField.find('input').prop('disabled', checked);
+      $latePenaltyField.find('select').prop('disabled', checked);
+      if (checked) {
+        $('#assessment_late_penalty_attributes_value').val('');
+      }
     });
 
     $('#use_default_version_threshold').on('change', function() {
-      $('#assessment_version_threshold').prop('disabled', $(this).prop('checked'));
+      const checked = $(this).prop('checked');
+      const $version_threshold = $('#assessment_version_threshold');
+      $version_threshold.prop('disabled', checked);
+      if (checked) {
+        $version_threshold.val('');
+      }
     });
 
     $('#use_default_version_penalty').on('change', function() {
+      const checked = $(this).prop('checked');
       const $versionPenaltyField = $('#assessment_version_penalty_attributes_value').parent();
-      $versionPenaltyField.find('input').prop('disabled', $(this).prop('checked'));
-      $versionPenaltyField.find('select').prop('disabled', $(this).prop('checked'));
+      $versionPenaltyField.find('input').prop('disabled', checked);
+      $versionPenaltyField.find('select').prop('disabled', checked);
+      if (checked) {
+        $('#assessment_version_penalty_attributes_value').val('');
+      }
     });
 
   });
