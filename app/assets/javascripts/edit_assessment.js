bc9bb9614439a088679fcfc7863243f31c103dfc
Michelle <mliuu3259@gmail.com>
2023-11-05 11:21:53 -0500

    resolve merge conflicts

--- app/assets/javascripts/edit_assessment.js
+++ app/assets/javascripts/edit_assessment.js
@@ -35,12 +35,10 @@
     $('#assessment_config_file').on('change', function () {
       var fileSelector = $("#assessment_config_file").get(0);
       var file = fileSelector.files[0];
-      const targetFile = $('#assessment_config_file').data('target-name');
 
-      if (file?.name !== targetFile) {
-        $('#config-file-type-incorrect').text(`Warning: ${file.name} doesn't match expected ${targetFile} file name`)
+      if (!file?.name?.endsWith('.rb')) {
+        $('#config-file-type-incorrect').text(`Warning: ${file.name} doesn't match expected .rb file type`)
       } else {
-        console.log('uhhh')
         $('#config-file-type-incorrect').text("")
       }
     })
@@ -56,6 +54,75 @@
       }
     });
 
+    // Penalties tab
+    let initial_load = true; // determines if the page is loading for the first time, if so, don't clear the fields
+
+    $('#unlimited_submissions').on('change', function() {
+      const checked = $(this).prop('checked');
+      const $max_submissions = $('#assessment_max_submissions');
+      $max_submissions.prop('disabled', checked);
+      if (checked) {
+        $max_submissions.val('Unlimited submissions');
+      } else if (!initial_load) {
+        $max_submissions.val('');
+      }
+    });
+
+    $('#unlimited_grace_days').on('change', function() {
+      const checked = $(this).prop('checked');
+      const $max_grace_days = $('#assessment_max_grace_days');
+      $max_grace_days.prop('disabled', checked);
+      if (checked) {
+        $max_grace_days.val('Unlimited grace days');
+      } else if (!initial_load) {
+        $max_grace_days.val('');
+      }
+    });
+
+    $('#use_default_late_penalty').on('change', function() {
+      const checked = $(this).prop('checked');
+      const $latePenaltyValue = $('#assessment_late_penalty_attributes_value');
+      const $latePenaltyField = $latePenaltyValue.parent();
+      $latePenaltyField.find('input').prop('disabled', checked);
+      $latePenaltyField.find('select').prop('disabled', checked);
+      if (checked) {
+        $latePenaltyValue.val('Course default');
+      } else if (!initial_load) {
+        $latePenaltyValue.val('');
+      }
+    });
+
+    $('#use_default_version_threshold').on('change', function() {
+      const checked = $(this).prop('checked');
+      const $version_threshold = $('#assessment_version_threshold');
+      $version_threshold.prop('disabled', checked);
+      if (checked) {
+        $version_threshold.val('Course default');
+      } else if (!initial_load) {
+        $version_threshold.val('');
+      }
+    });
+
+    $('#use_default_version_penalty').on('change', function() {
+      const checked = $(this).prop('checked');
+      const $versionPenaltyValue = $('#assessment_version_penalty_attributes_value');
+      const $versionPenaltyField = $versionPenaltyValue.parent();
+      $versionPenaltyField.find('input').prop('disabled', checked);
+      $versionPenaltyField.find('select').prop('disabled', checked);
+      if (checked) {
+        $versionPenaltyValue.val('Course default');
+      } else if (!initial_load) {
+        $versionPenaltyValue.val('');
+      }
+    });
+
+    // Trigger on page load
+    $('#unlimited_submissions').trigger('change');
+    $('#unlimited_grace_days').trigger('change');
+    $('#use_default_late_penalty').trigger('change');
+    $('#use_default_version_threshold').trigger('change');
+    $('#use_default_version_penalty').trigger('change');
+    initial_load = false;
   });
 
 })();
