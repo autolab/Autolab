925d581a8f8a1741151349e6861436e2b947582a
Damian Ho <damian_ho_xu_yang@yahoo.com>
2023-11-10 14:55:18 -0500

    Use update instead of update_attribute

--- db/migrate/20230924161219_add_release_at_to_attachments.rb
+++ db/migrate/20230924161219_add_release_at_to_attachments.rb
@@ -3,15 +3,15 @@ class AddReleaseAtToAttachments < ActiveRecord::Migration[6.0]
     add_column :attachments, :release_at, :datetime, default: -> { 'CURRENT_TIMESTAMP' }
     Attachment.find_each do |attachment|
       if attachment.released
-        attachment.update_attribute(:release_at, DateTime.now)
+        attachment.update(release_at: DateTime.now)
       else
-        attachment.update_attribute(:release_at, DateTime.now + 1.year)
+        attachment.update(release_at: DateTime.now + 1.year)
       end
     end
   end
   def down
     Attachment.find_each do |attachment|
-      attachment.update_attribute(:released, attachment.release_at < DateTime.now)
+      attachment.update(released: attachment.release_at < DateTime.now)
     end
     remove_column :attachments, :release_at
   end
