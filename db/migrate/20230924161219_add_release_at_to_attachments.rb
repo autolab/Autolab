e216c1f7ab0af5a419d3051d4da5417a0e3f7943
Damian Ho <damian_ho_xu_yang@yahoo.com>
2023-11-10 14:44:43 -0500

    Improve migration to be backwards compatible / reversible

--- db/migrate/20230924161219_add_release_at_to_attachments.rb
+++ db/migrate/20230924161219_add_release_at_to_attachments.rb
@@ -1,5 +1,18 @@
 class AddReleaseAtToAttachments < ActiveRecord::Migration[6.0]
-  def change
+  def up
     add_column :attachments, :release_at, :datetime, default: -> { 'CURRENT_TIMESTAMP' }
+    Attachment.find_each do |attachment|
+      if attachment.released
+        attachment.update_attribute(:release_at, DateTime.now)
+      else
+        attachment.update_attribute(:release_at, DateTime.now + 1.year)
+      end
+    end
+  end
+  def down
+    Attachment.find_each do |attachment|
+      attachment.update_attribute(:released, attachment.release_at < DateTime.now)
+    end
+    remove_column :attachments, :release_at
   end
 end
