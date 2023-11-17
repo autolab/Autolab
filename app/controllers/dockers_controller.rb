4570f332a5453667f653e3a25700ff3f8cc52713
Evan Shi <14984764+evanyeyeye@users.noreply.github.com>
2023-11-17 14:56:23 -0500

    Add success message

--- app/controllers/dockers_controller.rb
+++ app/controllers/dockers_controller.rb
@@ -8,13 +8,13 @@ class DockersController < ApplicationController
     imageFile = params["imageFile"]
     if imageFile.nil?
       flash[:error] = "Please select a docker image for uploading."
-      redirect_to(action: "index")
-      return
-    end
-    begin
-      TangoClient.build(imageFile.read)
-    rescue TangoClient::TangoException => e
-      flash[:error] = "Error while uploading docker image: #{e.message}"
+    else
+      begin
+        TangoClient.build(imageFile.read)
+        flash[:success] = "Successfully uploaded docker image"
+      rescue TangoClient::TangoException => e
+        flash[:error] = "Error while uploading docker image: #{e.message}"
+      end
     end
     redirect_to(action: "index")
   end
