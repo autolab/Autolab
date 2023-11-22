4a9f2d8ef5b5db590da35e19f9f06a2e884796a6
Damian Ho <damian_ho_xu_yang@yahoo.com>
2023-11-22 03:42:52 -0500

    Simplify toggleOptions implementation

--- app/assets/javascripts/moss.js
+++ app/assets/javascripts/moss.js
@@ -1,13 +1,14 @@
-function toggleOptions(dropdownId, tableId) {
-  const table = document.getElementById(tableId);
-  const dropdown = document.getElementById(dropdownId);
-  table.style.display = (table.style.display === "none") ? "block" : "none";
-  if (table.style.display === "none") {
-    $(dropdown).children('.expand-more').show()
-    $(dropdown).children('.expand-less').hide()
+function toggleOptions(dropdown, table) {
+  const $dropdown = $(dropdown);
+  const $table = $(table);
+
+  $table.toggle();
+  if ($table.is(':hidden')) {
+    $dropdown.children('.expand-more').show();
+    $dropdown.children('.expand-less').hide();
   } else {
-    $(dropdown).children('.expand-more').hide()
-    $(dropdown).children('.expand-less').show()
+    $dropdown.children('.expand-more').hide();
+    $dropdown.children('.expand-less').show();
   }
 }
 
