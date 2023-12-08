084fc5a87ab8e503ac94e33c4a101d25dae33632
Joey Wildman <josephwildman88@gmail.com>
2023-12-08 13:45:41 -0500

    update migration variable naming

--- db/migrate/20231208083728_add_version_number_to_assessment_user_data.rb
+++ db/migrate/20231208083728_add_version_number_to_assessment_user_data.rb
@@ -2,11 +2,11 @@ class AddVersionNumberToAssessmentUserData < ActiveRecord::Migration[6.0]
   def up
     add_column :assessment_user_data, :version_number, :integer
     AssessmentUserDatum.all.each do |aud|
-      test = Submission.where(course_user_datum_id: aud.course_user_datum_id, assessment_id: aud.assessment_id).maximum(:version)
-      unless test.nil?
-        aud.update(version_number: test)
-      else
+      max_submission_version = Submission.where(course_user_datum_id: aud.course_user_datum_id, assessment_id: aud.assessment_id).maximum(:version)
+      if max_submission_version.nil?
         aud.update(version_number: 0)
+      else
+        aud.update(version_number: test)
       end
     end
   end
