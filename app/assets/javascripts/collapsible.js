f8d98a5d4abcf0c790255a455b52173c1b3e7af8
Damian Ho <damian_ho_xu_yang@yahoo.com>
2024-01-05 01:26:51 +0800

    Coalesce accordions, simplify js, remove admin options

--- app/assets/javascripts/collapsible.js
+++ app/assets/javascripts/collapsible.js
@@ -1,14 +1,18 @@
-function toggleAria(id) {
-  let expanded = document.getElementById(id).getAttribute("aria-expanded");
-  if (expanded === "true") {
-    expanded = "false"
-  } else {
-    expanded = "true"
-  }
-  document.getElementById(id).setAttribute("aria-expanded", expanded);
-}
-
 $(document).ready(function(){
-  $('.collapsible').collapsible();
-  $('.collapsible').first().collapsible('open', 0);
+  const $collapsible = $('.collapsible');
+  $collapsible.collapsible({ accordion: false }); // Multiple items can be open at once
+
+  // Accessibility features
+  const $menuLink = $collapsible.find('.collapsible-menu-link');
+  $menuLink.attr('aria-expanded', false);
+  $menuLink.attr('role', 'button');
+  $menuLink.on('click keydown', function() {
+    $(this).attr("aria-expanded", function(_, attr){
+      return attr !== "true";
+    });
+  });
+
+  // Expand first item of each collapsible
+  $collapsible.collapsible('open', 0);
+  $collapsible.find('.collapsible-menu-link:first').attr('aria-expanded', 'true');
 });
