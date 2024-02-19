102a9a9ba05c3357deb08d09ac2b7ef8b617febd
Joey Wildman <josephwildman88@gmail.com>
2024-02-19 16:03:20 -0500

    Remove custom pluralize method in application_controller.rb (#2082)
    * Remove custom pluralize code as it is unnecessary and causing errors
    
    * remove pluralize again (got readded in merge)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -345,11 +345,6 @@ protected
   end
   ### END HELPERS
 
-  def pluralize(count, singular, plural = nil)
-    "#{count || 0} " +
-      (count == 1 || count =~ /^1(\.0+)?$/ ? singular : (plural || singular.pluralize))
-  end
-
   # make_dlist - Creates a string of emails that can be added as b/cc field.
   # @param section The section to email.  nil if we should email the entire
   # class.
