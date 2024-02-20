92eb9d668dceca29a0b362e9f865f7f17f0a469c
Evan Shi <14984764+evanyeyeye@users.noreply.github.com>
2024-02-20 01:33:51 -0500

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2024_02_19_175942) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
