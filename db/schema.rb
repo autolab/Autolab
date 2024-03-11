409e80cd22e0cfe943f37a0997a9afde8acf9bb0
Joanna Ge <joannage87@gmail.com>
2024-03-11 17:52:24 -0400

    fix schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_02_26_083719) do
+ActiveRecord::Schema.define(version: 2024_02_26_194217) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
