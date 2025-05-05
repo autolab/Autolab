add6c7613fd08bdd5d01dbdb524201051d1ed8ac
Aryan Jain <aryanj@andrew.cmu.edu>
2025-05-05 14:25:32 -0400

    minor schema fixes

--- db/schema.rb
+++ db/schema.rb
@@ -17,8 +17,6 @@ ActiveRecord::Schema.define(version: 2025_04_26_203028) do
     t.string "record_type", null: false
     t.integer "record_id", null: false
     t.integer "blob_id", null: false
-    t.integer "record_id", null: false
-    t.integer "blob_id", null: false
     t.datetime "created_at", null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
@@ -37,7 +35,6 @@ ActiveRecord::Schema.define(version: 2025_04_26_203028) do
   end
 
   create_table "active_storage_variant_records", force: :cascade do |t|
-    t.integer "blob_id", null: false
     t.integer "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
