068a34a553afd58373c8d95291e6ff9a06618fbc
Soma Narita <thedude6583@gmail.com>
2026-04-08 17:14:40 -0400

    autograding works with ec2docker now!

--- app/controllers/autograders_controller.rb
+++ app/controllers/autograders_controller.rb
@@ -37,6 +37,7 @@ class AutogradersController < ApplicationController
     tar_path = Rails.root.join("courses", @course.name, @assessment.name, "autograde.tar")
     @makefile_exists = File.exist?(makefile_path) ? makefile_path : nil
     @tar_exists = File.exist?(tar_path) ? tar_path : nil
+    @container_images = ContainerImage.where(course: @course).or(ContainerImage.where(is_public: true))
   end
 
   action_auth_level :update, :instructor
--- app/models/autograder.rb
+++ app/models/autograder.rb
@@ -15,7 +15,7 @@ class Autograder < ApplicationRecord
   validates :autograde_timeout,
             numericality: { greater_than_or_equal_to: 10, less_than_or_equal_to: 900 }
   validates :autograde_image, :autograde_timeout, presence: true
-  validates :autograde_image, length: { maximum: 64 }
+  validates :autograde_image, length: { maximum: 512 }
 
   with_options if: :use_access_key do
     validates :access_key_id, presence: true, format: {
