09f249e3283716ad9c6bd5a02c472c7415feb313
Kester <kestertan040@gmail.com>
2026-04-17 13:57:05 -0400

    remove t2 nano

--- app/models/autograder.rb
+++ app/models/autograder.rb
@@ -9,7 +9,7 @@ class Autograder < ApplicationRecord
   encrypts :access_key_id
 
   INSTANCE_TYPES = %w[
-    t2.nano t2.micro t2.small t2.medium t2.large t2.xlarge t2.2xlarge
+    t2.micro t2.small t2.medium t2.large t2.xlarge t2.2xlarge
     t3.micro t3.small t3.medium t3.large
     c5.large c5.xlarge c5.2xlarge
     r5.large r5.xlarge r5.2xlarge
