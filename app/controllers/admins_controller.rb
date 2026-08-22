0882032122aeda8f4baba0b52d0d775106655356
Giancarlo Umberto Ambrosino <129298577+Giabbi@users.noreply.github.com>
2026-08-22 20:39:21 +0200

    Feature: Add Global Admin-Defined EC2 Limits for Autograders (#2320)
    * Abstracted instance list and created backend controlled array for EC2 instance selection. TODO: connect array to admin panel so that admin can select which instance to allow
    
    * Added support for admin to restrict EC2 selection for instructors. Rolled back previous database schema change in favor of a global approach with a yaml config file
    
    * remove t2.nano from config/ec2_config.yml
    
    * changed the ec2 instance type selection for admins
    
    ---------
    
    Co-authored-by: Soma Narita <thedude6583@gmail.com>

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -42,6 +42,22 @@ class AdminsController < ApplicationController
         YAML.safe_load(File.read("#{Rails.configuration.config_location}/lti_config.yml"))
     end
 
+    ec2_config_path = "#{Rails.configuration.config_location}/ec2_config.yml"
+    ec2_instance_types_path = "#{Rails.configuration.config_location}/ec2_instance_types.yml"
+    @ec2_config_hash = if File.exist?(ec2_config_path) && File.size?(ec2_config_path)
+                         YAML.safe_load(File.read(ec2_config_path))
+                       else
+                         # Default to all instances if the admin hasn't configured it yet
+                         { "allowed_instances" => Autograder::INSTANCE_TYPES }
+                       end
+    @ec2_instance_types = if File.exist?(ec2_instance_types_path) &&
+                             File.size?(ec2_instance_types_path)
+                            YAML.safe_load(File.read(ec2_instance_types_path))
+                          else
+                            # Default to all instances if the admin hasn't configured it yet
+                            { "instance_types" => Autograder::INSTANCE_TYPES }
+                          end
+
     if Rails.cache.exist?(:tmp_smtp_config)
       @smtp_config_hash = Rails.cache.read(:tmp_smtp_config)
       Rails.cache.delete(:tmp_smtp_config)
--- app/controllers/autograders_controller.rb
+++ app/controllers/autograders_controller.rb
@@ -1,3 +1,4 @@
+##
 # Each Assessment can have an autograder, which is modified with this controller
 
 require 'pathname'
@@ -36,9 +37,19 @@ class AutogradersController < ApplicationController
     tar_path = Rails.root.join("courses", @course.name, @assessment.name, "autograde.tar")
     @makefile_exists = File.exist?(makefile_path) ? makefile_path : nil
     @tar_exists = File.exist?(tar_path) ? tar_path : nil
+
+    @allowed_instance_types = @course.allowed_ec2_instances.presence || ["t2.micro", "t3.micro"]
     @container_images = ContainerImage.ready.where(course: @course)
                                       .or(ContainerImage.ready.where(is_public: true))
                                       .order(created_at: :desc)
+    config_path = "#{Rails.configuration.config_location}/ec2_config.yml"
+
+    if File.exist?(config_path) && File.size?(config_path)
+      ec2_config_hash = YAML.safe_load(File.read(config_path))
+      @allowed_instance_types = ec2_config_hash["allowed_instances"] || Autograder::INSTANCE_TYPES
+    else
+      @allowed_instance_types = Autograder::INSTANCE_TYPES
+    end
   end
 
   action_auth_level :update, :instructor
